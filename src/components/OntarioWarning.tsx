d7386828a936cfa742455348187a10572a8f25fc
Dave Welsh <dave@qccareerschool.com>
2021-10-08 16:28:22 -0400

    Introduce a delay before showing Ontario warning

--- src/components/OntarioWarning.tsx
+++ src/components/OntarioWarning.tsx
@@ -1,3 +1,4 @@
+import { clearTimeout } from 'timers';
 import { useEffect } from 'react';
 import { useLocation } from '../hooks/useLocation';
 
@@ -14,16 +15,26 @@ export const OntarioWarning = (): null => {
   const location = useLocation();
 
   useEffect(() => {
+    // determine if we should show the warning
+    let showWarning = false;
     if (location?.countryCode === 'CA' && location?.provinceCode === 'ON') {
       if (window.sessionStorage) {
         // this way we won't show the message a second time if the user refreshes the page
         if (!sessionStorage.getItem('ontarioWarning')) {
           sessionStorage.setItem('ontarioWarning', '1');
-          alert(ontarioWarningMessage);
+          showWarning = true;
         }
       } else {
-        alert(ontarioWarningMessage);
+        showWarning = true;
       }
+    }
+
+    // show the warning after a small delay
+    if (showWarning) {
+      const timerId = setTimeout(() => {
+        alert(ontarioWarningMessage);
+      }, 1000);
+      return () => clearTimeout(timerId);
     }
   }, [ location ]);
 
