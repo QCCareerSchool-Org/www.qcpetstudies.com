2f96cb4766c43c730bafac8b319f7ff0fd91163d
avinashlakhera <avinashl@alohatechnology.com>
2021-10-18 15:49:36 +0530

    - updated  code to match layout

--- src/pages/dog-grooming-courses/resources/class-vs-online-dog-grooming-schools.tsx
+++ src/pages/dog-grooming-courses/resources/class-vs-online-dog-grooming-schools.tsx
@@ -5,13 +5,13 @@ import { DefaultLayout } from '../../../components/DefaultLayout';
 import { SEO } from '../../../components/SEO';
 import FirstSectionBackground from '../../../images/backgrounds/inclass_vs_online_bg.jpg';
 
-const LearningDuringCovid: NextPage = () => {
+const ClassVsOnlineLearning: NextPage = () => {
 
   return (
-    <DefaultLayout secondaryTitle="First Aid for Groomers Course">
+    <DefaultLayout>
       <SEO
         title="In-Class vs. Online Dog Grooming Schools"
-        description=""
+        description="In-Class vs. Online Dog Grooming Schools"
         canonical="dog-grooming-courses/resources/class-vs-online-dog-grooming-schools"
       />
 
@@ -37,11 +37,11 @@ const LearningDuringCovid: NextPage = () => {
             <div className="col-12 col-md-10 offset-md-1 mb-4">
               <h2>Admissions</h2>
             </div>
-            <div className="col-12 col-lg-5 mb-4 mb-lg-0 p-5 border border-light ">
+            <div className="col-10 col-lg-5 mb-4 mb-lg-0 p-5 border border-light ">
               <h3 className="d-flex align-items-center justify-content-center"><FaUniversity className="text-white bg-primary p-1 rounded-circle" />&nbsp;&nbsp;In-Class</h3>
               <p>Enroll by a specific date for the next year&apos;s classes</p>
             </div>
-            <div className="col-12 col-lg-5 mb-lg-0 p-5 border border-light">
+            <div className="col-10 col-lg-5 mb-lg-0 p-5 border border-light">
               <h3 className="d-flex justify-content-center align-items-center"><FaLaptop className="text-white bg-primary p-1 rounded-circle" />&nbsp;&nbsp;Online</h3>
               <p><strong>Enroll at any time.</strong> No admission dates or class schedules.</p>
             </div>
@@ -55,11 +55,11 @@ const LearningDuringCovid: NextPage = () => {
             <div className="col-12 col-md-10 offset-md-1 mb-4">
               <h2>Location</h2>
             </div>
-            <div className="col-12 col-lg-5 mb-4 mb-lg-0 p-5 bg-white">
+            <div className="col-10 col-lg-5 mb-4 mb-lg-0 p-5 bg-white">
               <h3 className="d-flex justify-content-center align-items-center"><FaUniversity className="text-white bg-primary p-1 rounded-circle" />&nbsp;&nbsp;In-Class</h3>
               <p>Specific campuses that are equipped with dog grooming facilities.</p>
             </div>
-            <div className="col-12 col-lg-5 mb-lg-0 p-5 bg-white">
+            <div className="col-10 col-lg-5 mb-lg-0 p-5 bg-white">
               <h3 className="d-flex justify-content-center align-items-center"><FaLaptop className="text-white bg-primary p-1 rounded-circle" />&nbsp;&nbsp;Online</h3>
               <p><strong>Study from anywhere;</strong> groom dogs at home or go to self-serve grooming areas.</p>
             </div>
@@ -73,11 +73,11 @@ const LearningDuringCovid: NextPage = () => {
             <div className="col-12 col-md-10 offset-md-1 mb-4">
               <h2>Instruction</h2>
             </div>
-            <div className="col-12 col-lg-5 mb-4 mb-lg-0 p-5 border border-light">
+            <div className="col-10 col-lg-5 mb-4 mb-lg-0 p-5 border border-light">
               <h3 className="d-flex justify-content-center align-items-center"><FaUniversity className="text-white bg-primary p-1 rounded-circle" />&nbsp;&nbsp;In-Class</h3>
               <p>Teacher gives in-person lectures on a strict schedule, and students read/study course books as homework.</p>
             </div>
-            <div className="col-12 col-lg-5 mb-lg-0 p-5 border border-light">
+            <div className="col-10 col-lg-5 mb-lg-0 p-5 border border-light">
               <h3 className="d-flex justify-content-center align-items-center"><FaLaptop className="text-white bg-primary p-1 rounded-circle" />&nbsp;&nbsp;Online</h3>
               <p>Students watch instructors on video (DVD or online) and read course books <strong>whenever it&apos;s convenient</strong>.</p>
             </div>
@@ -91,11 +91,11 @@ const LearningDuringCovid: NextPage = () => {
             <div className="col-12 col-md-10 offset-md-1 mb-4">
               <h2>Practice</h2>
             </div>
-            <div className="col-12 col-lg-5 mb-4 mb-lg-0 p-5 bg-white">
+            <div className="col-10 col-lg-5 mb-4 mb-lg-0 p-5 bg-white">
               <h3 className="d-flex justify-content-center align-items-center"><FaUniversity className="text-white bg-primary p-1 rounded-circle" />&nbsp;&nbsp;In-Class</h3>
               <p>Students work on dogs in a classroom environment with other students.</p>
             </div>
-            <div className="col-12 col-lg-5 mb-lg-0 p-5 bg-white">
+            <div className="col-10 col-lg-5 mb-lg-0 p-5 bg-white">
               <h3 className="d-flex justify-content-center align-items-center"><FaLaptop className="text-white bg-primary p-1 rounded-circle" />&nbsp;&nbsp;Online</h3>
               <p>Student <strong>practices from home</strong> as often as he/she feels appropriate to master skills.</p>            </div>
           </div>
@@ -108,11 +108,11 @@ const LearningDuringCovid: NextPage = () => {
             <div className="col-12 col-md-10 offset-md-1 mb-4">
               <h2>Finding Dogs</h2>
             </div>
-            <div className="col-12 col-lg-5 mb-4 mb-lg-0 p-5 border border-light">
+            <div className="col-10 col-lg-5 mb-4 mb-lg-0 p-5 border border-light">
               <h3 className="d-flex justify-content-center align-items-center"><FaUniversity className="text-white bg-primary p-1 rounded-circle" />&nbsp;&nbsp;In-Class</h3>
               <p>Students are provided with a &quot;class dog&quot; or are asked to find/bring their own dog to class.</p>
             </div>
-            <div className="col-12 col-lg-5 mb-lg-0 p-5 border border-light">
+            <div className="col-10 col-lg-5 mb-lg-0 p-5 border border-light">
               <h3 className="d-flex justify-content-center align-items-center"><FaLaptop className="text-white bg-primary p-1 rounded-circle" />&nbsp;&nbsp;Online</h3>
               <p>Student are asked to find <strong>their own dogs</strong> to groom.</p>
             </div>
@@ -126,11 +126,11 @@ const LearningDuringCovid: NextPage = () => {
             <div className="col-12 col-md-10 offset-md-1 mb-4">
               <h2>Theory Assignments</h2>
             </div>
-            <div className="col-12 col-lg-5 mb-4 mb-lg-0 p-5 bg-white">
+            <div className="col-10 col-lg-5 mb-4 mb-lg-0 p-5 bg-white">
               <h3 className="d-flex justify-content-center align-items-center"><FaUniversity className="text-white bg-primary p-1 rounded-circle" />&nbsp;&nbsp;In-Class</h3>
               <p>Students complete written assignments with strict deadlines. Tutor marks assignments &amp; occasionally discusses results with students.</p>
             </div>
-            <div className="col-12 col-lg-5 mb-lg-0 p-5 bg-white">
+            <div className="col-10 col-lg-5 mb-lg-0 p-5 bg-white">
               <h3 className="d-flex justify-content-center align-items-center"><FaLaptop className="text-white bg-primary p-1 rounded-circle" />&nbsp;&nbsp;Online</h3>
               <p>Students complete written assignments at their leisure with <strong>no assignment deadlines.</strong>
               &nbsp;Tutor grades assignments &amp; provides <strong>detailed audio feedback</strong> to each student.</p>
@@ -145,11 +145,11 @@ const LearningDuringCovid: NextPage = () => {
             <div className="col-12 col-md-10 offset-md-1 mb-4">
               <h2>Practical Assignments</h2>
             </div>
-            <div className="col-12 col-lg-5 mb-4 mb-lg-0 p-5 border border-light">
+            <div className="col-10 col-lg-5 mb-4 mb-lg-0 p-5 border border-light">
               <h3 className="d-flex justify-content-center align-items-center"><FaUniversity className="text-white bg-primary p-1 rounded-circle" />&nbsp;&nbsp;In-Class</h3>
               <p>Students complete full grooming assignments in-class. Tutor provides grade and feedback.</p>
             </div>
-            <div className="col-12 col-lg-5 mb-lg-0 p-5 border border-light">
+            <div className="col-10 col-lg-5 mb-lg-0 p-5 border border-light">
               <h3 className="d-flex justify-content-center align-items-center"><FaLaptop className="text-white bg-primary p-1 rounded-circle" />&nbsp;&nbsp;Online</h3>
               <p>Students complete<strong> full grooming assignments</strong> from home, submitting photos/videos of their work. Tutor provides grade and feedback.</p>
             </div>
@@ -163,11 +163,11 @@ const LearningDuringCovid: NextPage = () => {
             <div className="col-12 col-md-10 offset-md-1 mb-4">
               <h2>Program Completion</h2>
             </div>
-            <div className="col-12 col-lg-5 mb-4 mb-lg-0 p-5 bg-white">
+            <div className="col-10 col-lg-5 mb-4 mb-lg-0 p-5 bg-white">
               <h3 className="d-flex justify-content-center align-items-center"><FaUniversity className="text-white bg-primary p-1 rounded-circle" />&nbsp;&nbsp;In-Class</h3>
               <p>6 months – 2 years (depends on the program)</p>
             </div>
-            <div className="col-12 col-lg-5 mb-lg-0 p-5 bg-white">
+            <div className="col-10 col-lg-5 mb-lg-0 p-5 bg-white">
               <h3 className="d-flex justify-content-center align-items-center"><FaLaptop className="text-white bg-primary p-1 rounded-circle" />&nbsp;&nbsp;Online</h3>
               <p>Flexible – <strong>Complete the program at your own pace.</strong> (average 6 months – 1 year)</p>
             </div>
@@ -181,11 +181,11 @@ const LearningDuringCovid: NextPage = () => {
             <div className="col-12 col-md-10 offset-md-1 mb-4">
               <h2>Business Training</h2>
             </div>
-            <div className="col-12 col-lg-5 mb-4 mb-lg-0 p-5 border border-light">
+            <div className="col-10 col-lg-5 mb-4 mb-lg-0 p-5 border border-light">
               <h3 className="d-flex justify-content-center align-items-center"><FaUniversity className="text-white bg-primary p-1 rounded-circle" />&nbsp;&nbsp;In-Class</h3>
               <p>Not usually included</p>
             </div>
-            <div className="col-12 col-lg-5 mb-lg-0 p-5 border border-light">
+            <div className="col-10 col-lg-5 mb-lg-0 p-5 border border-light">
               <h3 className="d-flex justify-content-center align-items-center"><FaLaptop className="text-white bg-primary p-1 rounded-circle" />&nbsp;&nbsp;Online</h3>
               <p><strong> Business Training</strong></p>
             </div>
@@ -199,12 +199,12 @@ const LearningDuringCovid: NextPage = () => {
             <div className="col-12 col-md-10 offset-md-1 mb-4">
               <h2>Tuition Cost</h2>
             </div>
-            <div className="col-12 col-lg-5 mb-4 mb-lg-0 p-5 bg-white">
+            <div className="col-10 col-lg-5 mb-4 mb-lg-0 p-5 bg-white">
               <h3 className="d-flex justify-content-center align-items-center"><FaUniversity className="text-white bg-primary p-1 rounded-circle" />&nbsp;&nbsp;In-Class</h3>
               <p>$3,000 – $5,000</p>
               <p>(average)</p>
             </div>
-            <div className="col-12 col-lg-5 mb-lg-0 p-5 bg-white">
+            <div className="col-10 col-lg-5 mb-lg-0 p-5 bg-white">
               <h3 className="d-flex justify-content-center align-items-center"><FaLaptop className="text-white bg-primary p-1 rounded-circle" />&nbsp;&nbsp;Online</h3>
               <p><strong>$1,000 – $3,000</strong></p>
               <p>(average)</p>
@@ -219,10 +219,10 @@ const LearningDuringCovid: NextPage = () => {
             <div className="col-12 col-md-10 offset-md-1 mb-4">
               <h2>Certification</h2>
             </div>
-            <div className="col-12 col-lg-5 mb-4 mb-lg-0 p-5 border border-light">
+            <div className="col-10 col-lg-5 mb-4 mb-lg-0 p-5 border border-light">
               <h3 className="d-flex justify-content-center align-items-center"><FaUniversity className="text-white bg-primary p-1 rounded-circle" />&nbsp;&nbsp;In-Class</h3>
               <p>Industry-Recognized Certificate of Completion</p>            </div>
-            <div className="col-12 col-lg-5 mb-lg-0 p-5 border border-light">
+            <div className="col-10 col-lg-5 mb-lg-0 p-5 border border-light">
               <h3 className="d-flex justify-content-center align-items-center"><FaLaptop className="text-white bg-primary p-1 rounded-circle" />&nbsp;&nbsp;Online</h3>
               <p><strong>Industry-Recognized Certificate of Completion</strong></p>
             </div>
@@ -233,4 +233,4 @@ const LearningDuringCovid: NextPage = () => {
   );
 };
 
-export default LearningDuringCovid;
+export default ClassVsOnlineLearning;
