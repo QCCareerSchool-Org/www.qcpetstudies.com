87494af86c2d207acc15e4114b7a68d22849c9e2
avinashlakhera <avinashl@alohatechnology.com>
2021-10-18 16:11:21 +0530

    - updated layout for small screen

--- src/pages/dog-grooming-courses/resources/learning-and-working-during-covid.tsx
+++ src/pages/dog-grooming-courses/resources/learning-and-working-during-covid.tsx
@@ -86,17 +86,17 @@ const LearningDuringCovid: NextPage = () => {
             </div>
           </div>
           <div className="row justify-content-around mb-4">
-            <div className="col-12 col-lg-3 mb-4 mb-lg-0 p-5 border border-light">
+            <div className="col-10 col-lg-3 mb-4 mb-lg-0 p-5 border border-light">
               <FaTruck className="h2 text-secondary mb-3" />
               <h4>Mobile Grooming</h4>
               <p>Consider launching a <Link href="/2020/10/dog-grooming-career-pros-and-cons-of-mobile-grooming/"><a className="link-primary">mobile grooming</a></Link> service</p>
             </div>
-            <div className="col-12 col-lg-3 mb-4 mb-lg-0 p-5 border border-light">
+            <div className="col-10 col-lg-3 mb-4 mb-lg-0 p-5 border border-light">
               <FaLaptop className="h2 text-secondary mb-3" />
               <h4>Virtual Services</h4>
               <p>Introduce virtual services to your business, such as consultations and at-home grooming lessons</p>
             </div>
-            <div className="col-12 col-lg-3 p-5 border border-light">
+            <div className="col-10 col-lg-3 p-5 border border-light">
               <FaMedkit className="h2 text-secondary mb-3" />
               <h4>Protection</h4>
               <p>Insist that everyone (staff and clients) wear masks at all times</p>
@@ -104,17 +104,17 @@ const LearningDuringCovid: NextPage = () => {
           </div>
 
           <div className="row justify-content-around mb-4">
-            <div className="col-12 col-lg-3 mb-4 mb-lg-0 p-5 border border-light">
+            <div className="col-10 col-lg-3 mb-4 mb-lg-0 p-5 border border-light">
               <FaPencilAlt className="h2 text-secondary mb-3" />
               <h4>Blogging</h4>
               <p>Write for a dog grooming blog/vlog or start your own</p>
             </div>
-            <div className="col-12 col-lg-3 mb-4 mb-lg-0 p-5 border border-light">
+            <div className="col-10 col-lg-3 mb-4 mb-lg-0 p-5 border border-light">
               <FaInstagram className="h2 text-secondary mb-3" />
               <h4>Social Media</h4>
               <p>Become an authority on dog grooming on social media</p>
             </div>
-            <div className="col-12 col-lg-3 p-5 border border-light bg-primary text-white">
+            <div className="col-10 col-lg-3 p-5 border border-light bg-primary text-white">
               <FaPaw className="h2 text-secondary mb-3" />
               <h4 className="text-white">Sniffin&apos; Around</h4>
               <p>Visit QC&apos;s blog for articles and tips for working and expanding business in 2021</p>
@@ -127,7 +127,7 @@ const LearningDuringCovid: NextPage = () => {
         <Image src={TipsCardBackground} layout="fill" objectFit="cover" objectPosition="center" alt="Tips to keep safe" />
         <div className="container">
           <div className="row">
-            <div className="col-12 col-md-10 offset-md-1">
+            <div className="col-12 col-md-8 offset-md-1">
               <h2>Tips for Keeping Yourself and Clients Safe</h2>
               <h4>When working with clients in-person, you should follow strict practices to keep everyone safe.</h4>
               <h3>COVID-19 Safety Protocols for Dog Groomers</h3>
