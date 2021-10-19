60e11d7c3fc345d505bb017d771e9b5a4ca69c56
avinashlakhera <avinashl@alohatechnology.com>
2021-10-19 15:43:58 +0530

    - added link in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -56,6 +56,7 @@ export const Header = ({ noHero, secondaryTitle, secondaryNavLinks, className }:
                   <Link href="/dog-grooming-courses/dog-grooming#tuition"><a className="dropdown-item dropdown-item-sub">Tuition</a></Link>
                   <Link href="/dog-grooming-courses/first-aid"><a className="dropdown-item dropdown-item-primary">First Aid Course</a></Link>
                   <Link href="/dog-grooming-courses/breed-styling"><a className="dropdown-item dropdown-item-primary">Breed Styling Course</a></Link>
+                  <Link href="/dog-grooming-courses/resources/license-vs-certification"><a className="dropdown-item dropdown-item-primary">License VS Certification</a></Link>
                   <hr />
                   <Link href="/dog-grooming-courses/how-it-works"><a className="dropdown-item"><FaCog className="text-primary me-2" /> How It Works</a></Link>
                   <Link href="/dog-grooming-courses/resources"><a className="dropdown-item"><FaFolderOpen className="text-primary me-2" /> Dog Grooming Resources</a></Link>
