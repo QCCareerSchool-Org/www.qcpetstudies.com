b3dfb1a431710032e419c85de64885f60a0ebaf7
avinashlakhera <avinashl@alohatechnology.com>
2021-10-21 11:25:04 +0530

    - commited inital changes

--- src/pages/dog-grooming-courses/dog-grooming/index.tsx
+++ src/pages/dog-grooming-courses/dog-grooming/index.tsx
@@ -5,22 +5,25 @@ import { DefaultLayout } from '../../../components/DefaultLayout';
 import { PriceSection } from '../../../components/PriceSection';
 import { SEO } from '../../../components/SEO';
 
-import guarantee1Year from '../../../images/1-year-guarantee-outlined.svg';
-import guarantee21Day from '../../../images/21-day-guarantee-outlined.svg';
-import tutorBackground from '../../../images/backgrounds/tutor-background.jpg';
-import firstAidBook from '../../../images/first-aid-book-white.jpg';
-import firstAidLogo from '../../../images/first-aid-logo.svg';
-import scissorsSet from '../../../images/scissor-set-1.jpg';
+import PersnolizeFeedbackImage from '../../../images/account-multiple-check-outline.svg';
+import LifeTimeAccessImage from '../../../images/all-inclusive.svg';
+import DogGroomingBackground from '../../../images/backgrounds/dog-grooming.jpg';
+import ContentCutImage from '../../../images/content-cut.jpg';
+import DogCourseMaterialsImage from '../../../images/dg-course-materials-manuals-kit-white.jpg';
+import MasterGroomerLisaImage from '../../../images/lisa-video-thumbnail.jpg';
 
-const secondaryNavLinks = [
-  { name: 'Foo', url: '#firstSection' },
-  { name: 'First Aid', url: '#firstAidSection' },
-  { name: 'Tutors', url: '#tutorSection' },
-];
+import MovieClapperImage from '../../../images/movie-clapper.svg';
+import OutlineImage from '../../../images/outline.svg';
+import MasterGroomerPaddyImage from '../../../images/paddy-video-thumbnail.jpg';
+import PlayBtnImage from '../../../images/play-btn.svg';
+
+import CourseMaterialsImage from '../../../images/playlist-check.svg';
 
 const DogGroomingPage: NextPage = () => {
+  const headerIconSize = 20;
+  const iconSize30 = 30;
   return (
-    <DefaultLayout secondaryTitle="Dog Grooming Course" secondaryNavLinks={secondaryNavLinks}>
+    <DefaultLayout>
       <SEO
         title="Dog Grooming Course"
         description=""
@@ -28,12 +31,30 @@ const DogGroomingPage: NextPage = () => {
       />
 
       <section id="firstSection" className="bg-dark">
-        <Image src={tutorBackground} layout="fill" objectFit="cover" objectPosition="center" alt="dddd" />
+        <Image src={DogGroomingBackground} layout="fill" objectFit="cover" objectPosition="center" alt="Dog Grooming" />
         <div className="container text-center">
+          <div className="row mb-4">
+            <h1>Dog Grooming</h1>
+            <h4>Get Started For Only <strong>$99</strong></h4>
+            <i>See tuition details</i>
+          </div>
+          <div className="row text-center mb-4">
+            <a href="#"><button className="btn btn-secondary btn-lg">Enroll Online</button></a>
+          </div>
           <div className="row">
-            <div className="col-12 col-lg-10 offset-lg-1">
-              <h1>Lorem Ipsum</h1>
-              <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <div className="d-flex justify-content-center gap-5">
+              <div className="text-uppercase">
+                <Image src={MovieClapperImage} alt="Movie Clapper" width={headerIconSize} height={headerIconSize} />
+                <p><strong>Trailer</strong></p>
+              </div>
+              <div className="text-uppercase">
+                <Image src={OutlineImage} alt="Outline" width={headerIconSize} height={headerIconSize} />
+                <p><strong>Outline</strong></p>
+              </div>
+              <div className="text-uppercase">
+                <Image src={PlayBtnImage} alt="Play Button" width={headerIconSize} height={headerIconSize} />
+                <p><strong>Sample</strong></p>
+              </div>
             </div>
           </div>
         </div>
@@ -41,13 +62,10 @@ const DogGroomingPage: NextPage = () => {
 
       <section>
         <div className="container text-center">
-          <div className="row">
-            <div className="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2">
-              <h2>Nam Tempus Vestibulum Gravida</h2>
-              <p>Donec felis erat, eleifend eu consequat vitae, semper a lectus. Maecenas luctus nisi augue, vitae aliquam velit vestibulum sit amet. Pellentesque orci tellus, convallis nec odio at, sollicitudin scelerisque risus. Vestibulum quis blandit massa. Maecenas vitae vestibulum ligula. Quisque ut sem nec sem semper suscipit vel at lectus. Donec diam sapien, hendrerit vel velit sit amet, accumsan consectetur arcu. Etiam gravida malesuada odio, vitae tempus ipsum cursus vel. Pellentesque lobortis nibh vel erat condimentum, vitae porttitor nisi fermentum. Proin condimentum quam nec tristique ultrices. Praesent placerat erat neque, non maximus odio ornare vel. Ut commodo, nibh non malesuada eleifend, tortor turpis pretium neque, sit amet auctor felis magna nec dolor.</p>
-              <button className="btn btn-primary-dark">Maximus Odio</button>
-            </div>
-          </div>
+          <h2>Become a <strong>Certified Dog Groomer</strong></h2>
+          <p className="lead"><strong>International Dog Grooming Professional | </strong> <p className="d-inline"> <i>IDGP </i></p></p>
+          <p>Get your International Dog Groomer Certification in less than a year with QC&apos;s interactive online training. Study at your own pace. Watch instructional videos and complete hands-on assignments to grow your grooming skills.  Graduate with all the knowledge and skills you need to succeed in the dog grooming industry!</p>
+          <p>Are you ready to start an amazing career?</p>
         </div>
       </section>
 
@@ -55,51 +73,146 @@ const DogGroomingPage: NextPage = () => {
 
       <section>
         <div className="container text-center">
-          <h2 className="mb-5">Suspendisse Viverra Elit Lacinia Eleifend Bibendum</h2>
+          <h2 className="mb-2 mb-md-4">Included in <strong>Your Course</strong></h2>
+          <Image src={DogCourseMaterialsImage} width={830} height={550} alt="Dg Course Materials" />
+          <div className="text-center mb-4">
+            <Image src={ContentCutImage} width={iconSize30} height={iconSize30} alt="Content Cut" />
+            <h3>Professional-Grade Grooming Starter Kit</h3>
+            <p>When you enroll, you&apos;ll receive a kit of dog grooming tools to help you complete your studies and start your career. This kit includes cordless WAHL clippers and combs, three grooming scissors, an assortment of brushes and combs, and more!</p>
+            <a href="#" className="text-uppercase"><strong> View Kit Details </strong></a>
+          </div>
           <div className="row">
-            <div className="col-12 col-lg-6 mb-5 mb-lg-0">
-              <div className="mb-4">
-                <Image src={guarantee21Day} width={165} height={165} alt="21-Day Money-Back Guarantee" />
+            <div className="d-flex flex-column flex-md-row gap-4">
+              <div>
+                <div className="mb-2 mb-md-4">
+                  <Image src={CourseMaterialsImage} width={iconSize30} height={iconSize30} alt="Newest Course Materials" />
+                </div>
+                <h3>Newest Course Materials</h3>
+                <p>Your course materials are always available online and are always being updated with the latest industry standards.  Refer to your updated training guides throughout your career!</p>
               </div>
-              <h3 className="h5">Quisque Placerat Odio Lectus</h3>
-              <p>Vel sodales nunc euismod a. Sed ut diam scelerisque, consequat massa a, vulputate leo. Morbi sollicitudin fringilla ex, nec rutrum turpis porttitor vel. Duis sit amet sem commodo, hendrerit odio vel, dapibus erat. Quisque eget magna volutpat, varius velit sed, sollicitudin est. Donec sit amet erat ut orci faucibus elementum. Duis augue tellus, fringilla eu nisl id, tristique dictum diam. Morbi sit amet augue ultrices, vehicula nisl at, congue augue. Vestibulum volutpat lectus erat, quis fringilla odio cursus non.</p>
-            </div>
-            <div className="col-12 col-lg-6">
-              <div className="mb-4">
-                <Image src={guarantee1Year} width={199} height={165} alt="1-Year Money-Back Guarantee" />
+              <div>
+                <div className="mb-2 mb-md-4">
+                  <Image src={PersnolizeFeedbackImage} width={iconSize30} height={iconSize30} alt="Personalized Feedback" />
+                </div>
+                <h3>Personalized Feedback</h3>
+                <p>Just because you&apos;re learning online doesn&apos;t mean you&apos;re learning alone.  You&apos;ll receive personalized audio feedback from your tutor on every dog grooming assignment you submit.</p>
               </div>
-              <h3 className="h5">Leo Sit Amet Sollicitudin Sollicitudin</h3>
-              <p>hasellus rhoncus volutpat ex a dapibus. Aenean id purus non nisi pretium bibendum. Donec dapibus sollicitudin nunc. Ut bibendum, leo sit amet sollicitudin sollicitudin, nisl ipsum sollicitudin ante, ut imperdiet tellus elit eget ligula. In at velit quis odio sodales sollicitudin. Nullam a magna sed sem gravida iaculis vitae a quam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus auctor risus in lorem porttitor, sed facilisis augue feugiat. Nullam et eleifend tellus.</p>
+              <div>
+                <div className="mb-2 mb-md-4">
+                  <Image src={LifeTimeAccessImage} width={iconSize30} height={iconSize30} alt="Lifetime Access" />
+                </div>
+                <h3>Lifetime Access</h3>
+                <p>Once you&apos;re a member of the QC family you &apos;ll have lifetime access to all the tools and resources available to QC students. This includes discounts on industry products and so much more!</p>
+              </div>
             </div>
           </div>
         </div>
       </section>
 
-      <section className="bg-dark">
+      <section className="bg-light">
         <div className="container text-center">
-          <div className="row">
-            <div className="col-12 col-md-10 offset-md-1">
-              <h2>Bonus Tools for Dog Grooming Students</h2>
-              <p className="h5">Professional Grooming Scissors</p>
-              <p>Before you begin your practical work in Unit C, you&apos;ll receive three pairs of professional grooming scissors that enable you to create a variety of looks.</p>
-              <Image src={scissorsSet} alt="scissors set" width={816} height={270} />
-              <p><em>Product may vary based on the supplier.</em></p>
+          <h2>Course <strong>Outline</strong></h2>
+          <p><strong>The Dog Grooming Course is Split into 5 parts, each containing individual training units.</strong></p>
+          <p>You must complete a unit with a satisfactory grade before you can submit assignment for the next units.</p>
+        </div>
+        <div className="row d-flex justify-content-center">
+          <div className="col-10">
+            <div className="card-header d-flex justify-content-between align-items-center p-3">
+              <h3 className="mb-0">Part 1</h3>
+              <a className="btn btn-primary" data-bs-togle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">+</a>
+              <div className="collapse" id="collapseExample">
+                <div className="card card-body">
+                  <p className="lead"><strong>Units A-C</strong></p>
+                  <p>The first few units in this course will introduce you to the fundamental concepts of dog grooming. In these units, you&apos;ll cover health and safety, basic dog anatomy, skincare, behavior, the tools of the trade and more.</p>
+                  <p>During these units it is recommended that you start considering your grooming workspace so that you are prepared for the practical assignments later in the course. Unit C will be particularly helpful in selecting appropriate tools, equipment and products.</p>
+                  <p>It is also recommended that you start seeking out dog owners who are willing to let you use their dogs in practical assignments later in the course.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row d-flex justify-content-center">
+          <div className="col-10">
+            <div className="card-header d-flex justify-content-between align-items-center p-3">
+              <h3 className="mb-0">Part 2</h3>
+              <a className="btn btn-primary" data-bs-togle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">+</a>
+              <div className="collapse" id="collapseExample">
+                <div className="card card-body">
+                  <p>In order to provide you with the resources to learn your craft safely, your grooming course also comes with a two-unit course on dog first aid. This course covers emergency situations that may occur in a grooming environment and prepares you to respond calmly and effectively in any emergency.</p>
+                  <p>You must complete both units of the First Aid course before proceeding to the next unit in the dog grooming course.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row d-flex justify-content-center">
+          <div className="col-10">
+            <div className="card-header d-flex justify-content-between align-items-center p-3">
+              <h3 className="mb-0">Part 3</h3>
+              <a className="btn btn-primary" data-bs-togle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">+</a>
+              <div className="collapse" id="collapseExample">
+                <div className="card card-body">
+                  <p>Once you understand the basics of dog grooming and dog first aid, you will move on to units that teach you the hands-on skills involved in grooming. In these units, you&apos;ll cover nail trimming, ear cleaning, brushing, de-matting, bathing, drying, coat types, pet cuts, breed standards and more. You&apos;ll complete a number of basic practical assignments that will prepare you to start your practicum.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row d-flex justify-content-center">
+          <div className="col-10">
+            <div className="card-header d-flex justify-content-between align-items-center p-3">
+              <h3 className="mb-0">Part 4</h3>
+              <a className="btn btn-primary" data-bs-togle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">+</a>
+              <div className="collapse" id="collapseExample">
+                <div className="card card-body">
+                  <p>Dog grooming as a profession requires a lot of background knowledge, but ultimately your success will depend on your ability to safely and skillfully groom dogs. Your practicum is designed to allow you to practice and receive feedback on these essential skills. The units within the practicum involve intensive practical assignments that draw on the content you&apos;ve mastered throughout the course. You&apos;ll have a chance to receive feedback from your tutors on your bathing and drying skills, in addition to the pet cuts and breed cuts you&apos;ll create throughout your career.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row d-flex justify-content-center">
+          <div className="col-10">
+            <div className="card-header d-flex justify-content-between align-items-center p-3">
+              <h3 className="mb-0">Part 5</h3>
+              <a className="btn btn-primary" data-bs-togle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">+</a>
+              <div className="collapse" id="collapseExample">
+                <div className="card card-body">
+                  <p>Dog grooming as a profession requires a lot of background knowledge, but ultimately your success will depend on your ability to safely and skillfully groom dogs. Your practicum is designed to allow you to practice and receive feedback on these essential skills. The units within the practicum involve intensive practical assignments that draw on the content you&apos;ve mastered throughout the course. You&apos;ll have a chance to receive feedback from your tutors on your bathing and drying skills, in addition to the pet cuts and breed cuts you&apos;ll create throughout your career.</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </section>
 
-      <section id="firstAidSection">
-        <div className="container text-center">
-          <div className="row align-items-center">
-            <div className="col-12 col-md-10 offset-md-1 col-lg-6 offset-lg-0 mb-2 mb-lg-0 text-lg-start">
-              <Image src={firstAidLogo} alt="First Aid Course logo" width={60} height={60} />
-              <h2>Get a <strong>FREE</strong> First Aid Course</h2>
-              <p className="lead">Enroll in Dog Grooming &amp; receive the First Aid for Dog Groomers course to take your career to the next level!</p>
-              <p>Being trained in Pet First Aid prepares you to respond to emergencies and maintain a safe grooming environment. You&apos;ll learn from dog first aid experts who will teach you what you need to know to keep yourself and your furry clients out of harm&apos;s way, and how to deal with emergency situations if they arise.</p>
-            </div>
-            <div className="col-12 col-lg-6" style={{ fontSize: 0, marginBottom: '-20px' }}>
-              <Image src={firstAidBook} alt="First Aid book" width={504} height={385} />
+      <section>
+        <div className="container">
+          <div className="row">
+            <div className="d-flex flex-column flex-md-row justify-content-center gap-4">
+              <div className="col-12 col-md-5">
+                <h2>Learne from Certified <strong>Master Groomers</strong></h2>
+                <p>When you enroll with QC Pet Studies, you&apos;ll be matched with an industry professional currently working in the field who will review your work and provide you with in-depth feedback and advice on every assignment. These accomplished tutors are Certified Master Groomers who have decades of experience in the grooming industry and are eager to share their professional insight to help you succeed.</p>
+                <p>Your personal tutor will provide you with in-depth audio feedback on each assignment to let you know what you did well and how you could improve. Use this feedback to guide you through the next unit in your course.</p>
+              </div>
+              <div className="col-12 col-md-5 d-flex flex-column gap-5">
+                <div className="d-flex gap-4 align-items-center">
+                  <Image src={MasterGroomerLisaImage} alt="Master groomer" width={200} height={150} />
+                  <div>
+                    <h3>Lisa Day</h3>
+                    <h4>Certified Master Groomer</h4>
+                    <i>40+ Years of Experience</i>
+                  </div>
+                </div>
+                <div className="d-flex gap-4 align-items-center">
+                  <Image src={MasterGroomerPaddyImage} alt="Master groomer" width={200} height={150} />
+                  <div>
+                    <h3>Paddy Gaffney</h3>
+                    <h4>Certified Master Groomer</h4>
+                    <i>20 Years of Experience</i>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
