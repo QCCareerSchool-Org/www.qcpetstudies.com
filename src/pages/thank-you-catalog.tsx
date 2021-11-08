36b2de5fe7907ee53f3651aac538f109a2ec4f07
Dave Welsh <dave@qccareerschool.com>
2021-11-08 13:59:01 -0500

    Update thank you page

--- src/pages/thank-you-catalog.tsx
+++ src/pages/thank-you-catalog.tsx
@@ -1,39 +1,34 @@
 import { NextPage } from 'next';
 import Image from 'next/image';
 import { FaPaw } from 'react-icons/fa';
+
 import { DefaultLayout } from '../components/DefaultLayout';
 import { SEO } from '../components/SEO';
-import CatalogBackground from '../images/backgrounds/CC-Thankyou-bg.jpg';
+import CatalogBackground from '../images/backgrounds/white-poodle-circle-cut.jpg';
 
-const ThankYouCatalogPage: NextPage = () => {
+const ThankYouCatalogPage: NextPage = () => (
+  <DefaultLayout>
+    <SEO
+      title="Here's Your Course Catalog"
+      description="Here's Your Course Catalog"
+      canonical="/thank-you-catalog"
+    />
 
-  return (
-    <DefaultLayout>
-      <SEO
-        title="Here's Your Course Catalog"
-        description="Here's Your Course Catalog"
-        canonical="/thank-you-catalog"
-      />
-
-      <section id="firstSection" className="bg-dark">
-        <Image src={CatalogBackground} layout="fill" objectFit="cover" objectPosition="center" alt="Dog looking at you" />
-        <div className="container text-center">
-          <h1 className="mb-4">Your course catalog</h1>
-          <h2>Thank You For Your Interest!</h2>
-          <p className="lead"><strong>Find out what a career in dog grooming can do for you, and how you can get started with QC&apos;s Dog Grooming course.</strong></p>
-          <div className="row justify-content-center">
-            <h4 className="my-4">Download your course catalog here:</h4>
-            <a href="">
-              <button className="btn btn-outline-secondary btn-lg text-white">Download&nbsp;&nbsp;
-                <FaPaw />
-              </button>
-            </a>
+    <section id="firstSection" className="bg-dark text-shadow">
+      <Image src={CatalogBackground} layout="fill" objectFit="cover" objectPosition="center" placeholder="blur" alt="white poodle with circle cut" />
+      <div className="container text-center">
+        <div className="row justify-content-center">
+          <div className="col-12 col-lg-10">
+            <h1 className="mb-4">Your course catalog</h1>
+            <h2>Thank You for Your Interest!</h2>
+            <p className="lead">Find out what a career in dog grooming can do for you, and how you can get started with QC&apos;s Dog Grooming course.</p>
+            <h4 className="mb-4">Download your course catalog here:</h4>
+            <a href="" className="btn btn-outline-secondary btn-lg">Download&nbsp;&nbsp;<FaPaw /></a>
           </div>
         </div>
-      </section>
-
-    </DefaultLayout>
-  );
-};
+      </div>
+    </section>
+  </DefaultLayout>
+);
 
 export default ThankYouCatalogPage;
