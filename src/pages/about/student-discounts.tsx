abf31af0d70237986f414f1c29d6628c4535aa36
Dave Welsh <dave@qccareerschool.com>
2021-11-09 12:37:29 -0500

    Update Student Discount page
    Rename image files
    Use column layout for static-width images

--- src/pages/about/student-discounts.tsx
+++ src/pages/about/student-discounts.tsx
@@ -1,141 +1,111 @@
 import { NextPage } from 'next';
 import Image from 'next/image';
+import { ReactElement, ReactNode } from 'react';
+
 import { DefaultLayout } from '../../components/DefaultLayout';
 import { SEO } from '../../components/SEO';
 import StudentDiscountBackground from '../../images/backgrounds/student-discounts-bg.jpg';
-import DirectLogo from '../../images/directLogo.jpg';
-import EpiPetLogo from '../../images/EpiPet.jpg';
-import MasonCompanyLogo from '../../images/MasonCo_logo.jpg';
-import MyTimeLogo from '../../images/mytime_logo.jpg';
-import PetSoftwareLogo from '../../images/pet-software-logo.jpg';
-import SenseiWhiteLogo from '../../images/SenseiWhiteBGLog.jpg';
-import SharkFinLogo from '../../images/SharkFinLogo.jpg';
-import ZenDogLogo from '../../images/zendog.jpg';
+import DaySmartPetLogo from '../../images/logos/day-smart-pet.svg';
+import DirectLogo from '../../images/logos/direct-animal-products.jpg';
+import EpiPetLogo from '../../images/logos/epi-pet.jpg';
+import MasonCompanyLogo from '../../images/logos/mason-company.jpg';
+import MyTimeLogo from '../../images/logos/mytime.svg';
+import SenseiLogo from '../../images/logos/sensei-shear-systems.jpg';
+import SharkFinLogo from '../../images/logos/shark-fin.jpg';
+import ZenDogLogo from '../../images/logos/zen-dog.jpg';
 
-const StudentDiscountsPage: NextPage = () => {
+const StudentDiscountsPage: NextPage = () => (
+  <DefaultLayout>
+    <SEO
+      title="Student Discounts"
+      description="This is Student Discount page"
+      canonical="/about/student-discounts"
+    />
 
-  return (
-    <DefaultLayout>
-      <SEO
-        title="Student Discount"
-        description="This is Student Discount page"
-        canonical="/student-discount"
-      />
-
-      <section id="firstSection" className="bg-dark">
-        <Image src={StudentDiscountBackground} layout="fill" objectFit="cover" objectPosition="center" alt="Dog looking at you" />
-        <div className="container">
-          <h2 className="text-uppercase">QC Student<br /><strong>Discounts</strong></h2>
+    <section id="firstSection" className="bg-dark">
+      <Image src={StudentDiscountBackground} layout="fill" objectFit="cover" objectPosition="center" alt="Yorkie sticking out its tongue" />
+      <div className="container text-center">
+        <div className="row justify-content-center">
+          <div className="col-12 col-lg-10">
+            <h1>QC Student Discounts</h1>
+            <p className="lead">QC partners with top quality vendors to provide amazing discounts for our students.</p>
+            <p className="mb-0">Below you&apos;ll find a list of QC&apos;s preferred partners who offer special discounts to QC Pet Studies&apos; students and graduates. In most cases, these offers are available to you as soon as you enroll!</p>
+          </div>
         </div>
-      </section>
+      </div>
+    </section>
 
-      <section>
-        <div className="container text-center">
-          <div className="row justify-content-center">
-            <div className="col-12 col-lg-10">
-              <p className="lead"><strong>QC partners with top quality vendors to provide amazing discounts for our students</strong></p>
-              <p>Below you&apos;ll find a list of QC&apos;s preferred partners who offer special discounts to QC Pet Studies&apos; students and graduates. In most cases, these offers are available to you as soon as you enroll!</p>
-            </div>
-          </div>
-
-          <h2 className="my-5">Pet Business <strong>Discounts</strong></h2>
-
-          <div className="row mb-4 align-items-center">
-            <div className="col-12 col-lg-3 mb-3">
-              <Image src={PetSoftwareLogo} alt="123Pet software logo" height="130px" width="170px" />
-            </div>
-            <div className="col-12 col-lg-9 text-lg-start">
-              <p className="lead"><strong>123Pet Software</strong></p>
-              <p>123Pet Software develops, delivers, and supports powerful business management software that runs thousands of pet businesses around the world. You&apos;ll be able to run your dog grooming business efficiently and grow your business with unique software features!</p>
-              <em>QC Pet Studies students receive a 50% off discount for 2 months</em>
-            </div>
-          </div>
-
-          <div className="row mb-4 align-items-center">
-            <div className="col-12 col-lg-3">
-              <Image src={MyTimeLogo} alt="My time logo" objectFit="none" />
-            </div>
-            <div className="col-12 col-lg-9 text-lg-start">
-              <p className="lead"><strong>MyTime</strong></p>
+    <section>
+      <div className="container text-center">
+        <div className="row justify-content-center">
+          <div className="col-12 col-lg-10">
+            <h2 className="mb-4">Pet Business <strong>Discounts</strong></h2>
+            <Supplier imageSrc={DaySmartPetLogo} imageWidth={170} imageHeight={46} imageAlt="DaySmart Pet" className="mb-5">
+              <h3>DaySmart Pet</h3>
+              <p>DaySmart Pet develops, delivers, and supports powerful business management software that runs thousands of pet businesses around the world. You&apos;ll be able to run your dog grooming business efficiently and grow your business with unique software features!</p>
+              <p className="mb-0"><em>QC Pet Studies students receive a 50% off discount for 2 months</em></p>
+            </Supplier>
+            <Supplier imageSrc={MyTimeLogo} imageWidth={170} imageHeight={69} imageAlt="MyTime" className="mb-5">
+              <h3>MyTime</h3>
               <p>MyTime is the leading cloud based scheduling and marketing application for local businesses. Features include online booking, automated email and SMS marketing, full point of sale, reputation management, reports and analytics, and real time messaging with clients. Use MyTime to engage and retain customers, drive operational efficiency and grow your business!</p>
-              <em>QC Pet Studies students receive 30% off their purchase</em>
-            </div>
-          </div>
-          <div className="row mb-4 align-items-center">
-            <div className="col-12 col-lg-3">
-              <Image src={MasonCompanyLogo} alt="Mason company logo" objectFit="none" />
-            </div>
-            <div className="col-12 col-lg-9 text-lg-start">
-              <p className="lead"><strong>Mason Company</strong></p>
+              <p className="mb-0"><em>QC Pet Studies students receive 30% off their purchase</em></p>
+            </Supplier>
+            <Supplier imageSrc={MasonCompanyLogo} imageWidth={170} imageHeight={51} imageAlt="Mason Company" className="mb-5">
+              <h3>Mason Company</h3>
               <p>Mason Company is unique in its ability to design and build any size animal housing solution for dogs, cats and other animals. Their exclusive focus on animal enclosure solutions since 1892 means they deliver high quality animal enclosure solutions for even the toughest environments. They have installed more kennel systems than any other company in the world!</p>
-              <em>QC Pet Studies students and graduates receive 15% off their purchase</em>
-            </div>
-          </div>
-          <hr />
-        </div>
-      </section>
-
-      <section className="py-0">
-        <div className="container text-center">
-          <h2 className="mb-5">Grooming Tool <strong>Discounts</strong></h2>
-          <div className="row mb-4 align-items-center">
-            <div className="col-12 col-lg-3">
-              <Image src={SharkFinLogo} alt="Shark fin logo" objectFit="none" />
-            </div>
-            <div className="col-12 col-lg-9 text-lg-start">
-              <p className="lead"><strong>Shark Fin</strong></p>
+              <p className="mb-0"><em>QC Pet Studies students and graduates receive 15% off their purchase</em></p>
+            </Supplier>
+            <h2 className="mb-4">Grooming Tool <strong>Discounts</strong></h2>
+            <Supplier imageSrc={SharkFinLogo} imageWidth={170} imageHeight={65} imageAlt="Shark Fin" className="mb-5">
+              <h3>Shark Fin</h3>
               <p>The Shark Fin Shear Company aims to offer you the most technologically advanced shears in the world. Their patented technology has been tested for over 17 years throughout the world and is proven to dramatically help you with your scissor work.</p>
-              <em>QC Pet Studies students receive a 20% off discount on shears</em>
-            </div>
-          </div>
-          <div className="row mb-4 align-items-center">
-            <div className="col-12 col-lg-3 text-lg-start">
-              <Image src={SenseiWhiteLogo} alt="Sensei logo" objectFit="none" />
-            </div>
-            <div className="col-12 col-lg-9 text-lg-start">
-              <p className="lead"><strong>Sensei</strong></p>
+              <p className="mb-0"><em>QC Pet Studies students receive a 20% off discount on shears</em></p>
+            </Supplier>
+            <Supplier imageSrc={SenseiLogo} imageWidth={170} imageHeight={42} imageAlt="Sensei" className="mb-5">
+              <h3>Sensei</h3>
               <p>Sensei consistently designs and manufactures extremely ergonomic professional shears. With over thirty years of high performance blade technology, Sensei continues to strive to be an innovative force in the industry. They offer a vast collection of high quality Japanese shears including neutral grip crane handle shears, swivel shears, texture thinning shears and seamless blending shears that leave no line and comb out with zero drag.</p>
-              <em>QC Pet Studies students receive $25 or 10-15% (whichever is greater) off any shears purchased online, as well as free shipping within the USA. Students also receive an additional discount if they purchase multiple shears</em>
-            </div>
-          </div>
-          <div className="row mb-4 align-items-center">
-            <div className="col-12 col-lg-3 text-lg-start">
-              <Image src={EpiPetLogo} alt="Epi pet logo" objectFit="none" />
-            </div>
-            <div className="col-12 col-lg-9 text-lg-start">
-              <p className="lead"><strong>Epi Pet</strong></p>
-              <p>Epi Pet offers a variety of products that can help the professional groomer provide a complete and healthy groom and promote overall health for their clients&apos; pets. The Epi Pet Shampoo is multifunctional and promotes healthy skin and coat. The Epi Pet Ear Cleaner cleanses, enriches and refreshes the ear by gently removing dirt, grime and wax.</p>
-              <em>QC Pet Studies students receive 50% off products purchased on the Epi-Pet website</em>
-            </div>
-          </div>
-          <div className="row mb-4 align-items-center">
-            <div className="col-12 col-lg-3 text-lg-start">
-              <div className="d-flex justify-content-around">
-                <Image src={DirectLogo} alt="Direct animal product logo" height="150px" width="180px" objectPosition="right" />
-              </div>
-            </div>
-            <div className="col-12 col-lg-9 text-lg-start">
-              <p className="lead"><strong>Direct Animal Products</strong></p>
+              <p className="mb-0"><em>QC Pet Studies students receive $25 or 10-15% (whichever is greater) off any shears purchased online, as well as free shipping within the USA. Students also receive an additional discount if they purchase multiple shears</em></p>
+            </Supplier>
+            <Supplier imageSrc={EpiPetLogo} imageWidth={170} imageHeight={81} imageAlt="Epi-Pet" className="mb-5">
+              <h3>Epi-Pet</h3>
+              <p>Epi-Pet offers a variety of products that can help the professional groomer provide a complete and healthy groom and promote overall health for their clients&apos; pets. The Epi Pet Shampoo is multifunctional and promotes healthy skin and coat. The Epi Pet Ear Cleaner cleanses, enriches and refreshes the ear by gently removing dirt, grime and wax.</p>
+              <p className="mb-0"><em>QC Pet Studies students receive 50% off products purchased on the Epi-Pet website</em></p>
+            </Supplier>
+            <Supplier imageSrc={DirectLogo} imageWidth={170} imageHeight={49} imageAlt="Direct Animal Products" className="mb-5">
+              <h3>Direct Animal Products</h3>
               <p>Direct Animal Products supplies tubs, grooming tables and cages that minimize back, neck and shoulder strain. They understand that proper body mechanics along with ergonomic grooming equipment is absolutely essential to the longevity of a pet stylist. Their equipment promises to perform well and is designed with your comfort in mind.</p>
-              <em>QC Pet Studies students receive 10% off their online purchase</em>
-            </div>
-          </div>
-          <div className="row align-items-center mb-5">
-            <div className="col-12 col-lg-3 text-lg-start my-4">
-              <div className="d-flex justify-content-around">
-                <Image src={ZenDogLogo} alt="Zendog logo" height="170px" width="170px" />
-              </div>
-            </div>
-            <div className="col-12 col-lg-9 text-lg-start">
-              <p className="lead"><strong>ZenDog</strong></p>
+              <p className="mb-0"><em>QC Pet Studies students receive 10% off their online purchase</em></p>
+            </Supplier>
+            <Supplier imageSrc={ZenDogLogo} imageWidth={170} imageHeight={174} imageAlt="Zen Dog">
+              <h3>ZenDog</h3>
               <p>ZenDog sells earthly inspired aromatherapy shampoos, sprays, and gels that never contain synthetic fragrances, colors, bleaches, or dyes. They&apos;re formulated exclusively with 100% pure, therapeutic class essential oils. Their products are created by professional groomers for professional groomers.</p>
-              <em>QC Pet Studies students receive an Aromatherapy Package worth $160 for only $99. The package includes 10 different aromatherapy healing shampoos, sprays and gels</em>
-            </div>
+              <p className="mb-0"><em>QC Pet Studies students receive an Aromatherapy Package worth $160 for only $99. The package includes 10 different aromatherapy healing shampoos, sprays and gels</em></p>
+            </Supplier>
           </div>
         </div>
-      </section>
-    </DefaultLayout>
-  );
+      </div>
+    </section>
+  </DefaultLayout>
+);
+
+type SupplierProps = {
+  className?: string;
+  imageSrc: string;
+  imageWidth: number;
+  imageHeight: number;
+  imageAlt: string;
+  children: ReactNode;
 };
+
+const Supplier = ({ className, imageSrc, imageWidth, imageHeight, imageAlt, children }: SupplierProps): ReactElement => (
+  <div className={`d-flex flex-column flex-lg-row align-items-center ${className ? className : ''}`}>
+    <div className="mb-2 mb-lg-0 me-lg-5 flex-shrink-0">
+      <Image src={imageSrc} width={imageWidth} height={imageHeight} alt={imageAlt} />
+    </div>
+    <div className="text-lg-start">
+      {children}
+    </div>
+  </div>
+);
 
 export default StudentDiscountsPage;
