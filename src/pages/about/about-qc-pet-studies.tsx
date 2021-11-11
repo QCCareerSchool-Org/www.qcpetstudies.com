6af1884244749604d4b2fd3bb200324b9a62732d
Dave Welsh <dave@qccareerschool.com>
2021-11-11 10:11:58 -0500

    Update about page
    Combine paragraphs in guarantee
    Fix comma splice
    Update layout of questions?/contact us element

--- src/pages/about/about-qc-pet-studies.tsx
+++ src/pages/about/about-qc-pet-studies.tsx
@@ -5,6 +5,7 @@ import { ReactElement } from 'react';
 import { FaBookOpen, FaHeadphones, FaLightbulb, FaStar } from 'react-icons/fa';
 
 import { DefaultLayout } from '../../components/DefaultLayout';
+import { SEO } from '../../components/SEO';
 import Qc35Year from '../../images/35-year-emblem.svg';
 import QcBackground from '../../images/backgrounds/about-qc-background.jpg';
 import QcGuarantee from '../../images/double-guarantee.svg';
@@ -14,6 +15,11 @@ const AboutPage: NextPage = () => {
 
   return (
     <DefaultLayout>
+      <SEO
+        title="About QC Pet Studies"
+        description=""
+        canonical="/about/about-qc-pet-studies"
+      />
 
       <section id="firstSection" className="bg-dark">
         <Image src={QcBackground} layout="fill" objectFit="cover" objectPosition="center" placeholder="blur" alt="happy dog panting" />
@@ -65,7 +71,8 @@ const AboutPage: NextPage = () => {
               <div className="card">
                 <div className="card-body">
                   <h3 className="card-title">The 21-Day Money-Back Guarantee</h3>
-                  <p className="card-text">Try the course risk-free for 21 days. This gives you time to receive your course materials and evaluate whether the dog grooming course is right for you. If you decide it s not a good fit, simply contact QC to arrange a return of your course materials for a refund. It s that easy!<br />Note: For sanitary reasons, the clippers and attachment combs provided with the dog grooming course cannot be refunded (value $200 US)</p>
+                  <p className="card-text">Try the course risk-free for 21 days. This gives you time to receive your course materials and evaluate whether the dog grooming course is right for you. If you decide it s not a good fit, simply contact QC to arrange a return of your course materials for a refund. It&apos;s that easy!</p>
+                  <p className="card-text"><strong>Note:</strong> For sanitary reasons, the clippers and attachment combs provided with the dog grooming course cannot be refunded (value $200 US)</p>
                 </div>
               </div>
             </div>
@@ -73,15 +80,13 @@ const AboutPage: NextPage = () => {
               <div className="card">
                 <div className="card-body">
                   <h3 className="card-title">The 1-Year Money-Back Guarantee</h3>
-                  <p className="mb-0">Your online course will pay for itself within two years, that&apos;s a promise.</p>
-                  <p className="mb-0">That&apos;s right.  If after two years of receiving your certificate of completion, you haven&apos;t earned the equivalent of your tuition fees in your professional salary, your course tuition will be refunded in full.</p>
-                  <p className="mb-0">All you have to do is provide proof that you&apos;ve made a reasonable effort to find work as a dog groomer or dog trainer.</p>
-                  <p className="mb-0">Note: This guarantee is only valid for graduates of the online dog grooming or online dog training courses.</p>
+                  <p className="card-text">Your online course will pay for itself within two years. That&apos;s a promise. If after two years of receiving your certificate of completion, you haven&apos;t earned the equivalent of your tuition fees in your professional salary, your course tuition will be refunded in full. All you have to do is provide proof that you&apos;ve made a reasonable effort to find work as a dog groomer or dog trainer.</p>
+                  <p className="card-text"><strong>Note:</strong> This guarantee is only valid for graduates of the online dog grooming or online dog training courses.</p>
                 </div>
               </div>
             </div>
             <div className="col-12 col-lg-10">
-              <p className="alert alert-primary mb-0">Questions? <Link href="/contact-us"><a className="alert-link">Contact Us</a></Link></p>
+              <p className="lead">Questions about the guarantees? <Link href="/contact-us"><a className="link-primary">Contact Us</a></Link></p>
             </div>
           </div>
         </div>
