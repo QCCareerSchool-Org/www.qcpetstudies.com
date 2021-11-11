15e13e200abc775414d249f5403d3045e7ca25d2
avinashlakhera <avinashl@alohatechnology.com>
2021-11-11 15:39:38 +0530

    - updated page

--- src/pages/about/about-qc-pet-studies.tsx
+++ src/pages/about/about-qc-pet-studies.tsx
@@ -30,9 +30,9 @@ const AboutPage: NextPage = () => {
                 <div className="me-4 flex-shrink-0 mb-2 mb-md-0" style={{ width: '120px' }}>
                   <Image src={Qc35Year} layout="responsive" alt="distance education for over 35 years" />
                 </div>
-                <p className="lead mb-0">We&apos;re an online, international school that&apos;s been offering distance education courses since 1984, and in this time weve mastered the art of delivering quality course content online.</p>
+                <p className="lead mb-0">QC Pet Studies is a faculty of QC Career School. We&apos;re an online, international school that&apos;s been offering distance education courses since 1984, and in this time weve mastered the art of delivering quality course content online.</p>
               </div>
-              <p>QC Pet Studies offers the most comprehensive online dog grooming course available. Through lesson texts, video tutorials, hands on assignments, and personalized feedback from grooming professionals, our students receive the skills and resources they need to build a successful dog grooming career.</p>
+              <p>QC Pet Studies offers the most comprehensive online dog grooming course available. Through lesson texts, video tutorials, hands on assignments, and personalized feedback from industry professionals, our students receive the skills and resources they need to build successful careers in dog grooming and dog training.</p>
               <p>We combine our premium course content with our outstanding student support system. You can contact us anytime via phone, email, or Live Chat whichever you prefer. We are here to ensure that your course runs smoothly and that all of your needs are met with great care. Even after you graduate, we encourage you to stay in touch with us. We want to hear how you&apos;re doing and are always happy to provide you with career advice. When you enroll with QC Pet Studies, you join a supportive and ongoing community that is dedicated to helping you reach your goals.</p>
             </div>
           </div>
@@ -65,7 +65,7 @@ const AboutPage: NextPage = () => {
               <div className="card">
                 <div className="card-body">
                   <h3 className="card-title">The 21-Day Money-Back Guarantee</h3>
-                  <p className="card-text">Try the course risk-free for 21 days. This gives you time to receive your course materials and evaluate whether the dog grooming course is right for you. If you decide it s not a good fit, simply contact QC to arrange a return of your course materials for a refund. It s that easy! Note: For sanitary reasons, the clippers and attachment combs provided with the course cannot be refunded (value $200 US)</p>
+                  <p className="card-text">Try the course risk-free for 21 days. This gives you time to receive your course materials and evaluate whether the dog grooming course is right for you. If you decide it s not a good fit, simply contact QC to arrange a return of your course materials for a refund. It s that easy!<br />Note: For sanitary reasons, the clippers and attachment combs provided with the dog grooming course cannot be refunded (value $200 US)</p>
                 </div>
               </div>
             </div>
@@ -73,8 +73,10 @@ const AboutPage: NextPage = () => {
               <div className="card">
                 <div className="card-body">
                   <h3 className="card-title">The 1-Year Money-Back Guarantee</h3>
-                  <p className="card-text">This course will pay for itself within two years. That&apos;s a promise. If after two years of receiving your certificate of completion, you haven t earned the equivalent of your tuition fees in dog grooming fees or salary, your dog grooming course tuition will be refunded in full.</p>
-                  <p className="card-text">All you have to do is provide proof that you ve made a reasonable effort to find work as a dog groomer.</p>
+                  <p className="mb-0">Your online course will pay for itself within two years, that&apos;s a promise.</p>
+                  <p className="mb-0">That&apos;s right.  If after two years of receiving your certificate of completion, you haven&apos;t earned the equivalent of your tuition fees in your professional salary, your course tuition will be refunded in full.</p>
+                  <p className="mb-0">All you have to do is provide proof that you&apos;ve made a reasonable effort to find work as a dog groomer or dog trainer.</p>
+                  <p className="mb-0">Note: This guarantee is only valid for graduates of the online dog grooming or online dog training courses.</p>
                 </div>
               </div>
             </div>
@@ -113,7 +115,7 @@ const AboutPage: NextPage = () => {
                 right={(
                   <>
                     <h3>Online Course Innovation</h3>
-                    <p>QC offers the highest quality dog grooming course materials and student services available. From lesson texts to video tutorials to grooming tools, you&apos;ll see quality in every aspect of your course.</p>
+                    <p>QC Pet Studies enables you to learn online on your own time, at your own pace. You&apos;ll expand your knowledge and develop your practical skills while enjoying the flexibility and convenience of learning from home.</p>
                   </>
                 )}
               />
@@ -122,7 +124,7 @@ const AboutPage: NextPage = () => {
                 right={(
                   <>
                     <h3>Student Experience</h3>
-                    <p>QC offers the highest quality dog grooming course materials and student services available. From lesson texts to video tutorials to grooming tools, you&apos;ll see quality in every aspect of your course.</p>
+                    <p>At QC Pet Studies, our goal is to develop and continue personal relationships with each of our students. That&apos;s why we develop and maintain relationships with each and every dog grooming and dog training student throughout their course and after graduation, giving students a superior learning experience.</p>
                   </>
                 )}
               />
