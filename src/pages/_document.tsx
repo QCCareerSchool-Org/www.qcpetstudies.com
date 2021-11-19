c21f27fa68a849812999a19c6468f7f34a17d387
Dave Welsh <dave@qccareerschool.com>
2021-11-19 06:58:55 -0500

    Update code style

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -26,22 +26,22 @@ class MyDocument extends Document {
           />
           <script
             dangerouslySetInnerHTML={{ __html: `
-            piAId = '948642';
-            piCId = '34913';
-            piHostname = 'go.qcpetstudies.com';
+              piAId = '948642';
+              piCId = '34913';
+              piHostname = 'go.qcpetstudies.com';
 
-            (function() {
-              function async_load() {
-                var s = document.createElement('script'); s.type = 'text/javascript';
-                s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js';
-                var c = document.getElementsByTagName('script')[0]; c.parentNode.insertBefore(s, c);
-              }
-              if (window.attachEvent) {
-                window.attachEvent('onload', async_load);
-              } else {
-                window.addEventListener('load', async_load, false);
-              }
-            })();
+              (function() {
+                function async_load() {
+                  var s = document.createElement('script'); s.type = 'text/javascript';
+                  s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js';
+                  var c = document.getElementsByTagName('script')[0]; c.parentNode.insertBefore(s, c);
+                }
+                if (window.attachEvent) {
+                  window.attachEvent('onload', async_load);
+                } else {
+                  window.addEventListener('load', async_load, false);
+                }
+              })();
             ` }}
           />
           <script
