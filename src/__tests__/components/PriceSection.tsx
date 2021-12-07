8459335a2ba8386fcf95905d573632d2db5bf2ba
Dave Welsh <dave@qccareerschool.com>
2021-12-07 12:24:20 -0500

    Update tests

--- src/__tests__/components/PriceSection.tsx
+++ src/__tests__/components/PriceSection.tsx
@@ -10,12 +10,18 @@ describe('PriceSection component', () => {
 
   describe('invalidCourse', () => {
 
-    it('should return true if the courses are [ \'dg\' ] and the location is Ontario', () => {
+    it('should return true if the courses contain \'dg\' and the location is Ontario', () => {
+      expect(invalidCourse([ 'aa', 'dg', 'bb' ], 'CA', 'ON')).toBe(true);
       expect(invalidCourse([ 'dg' ], 'CA', 'ON')).toBe(true);
     });
 
-    it('should return true if the courses contain \'dg\' and the location is Ontario', () => {
-      expect(invalidCourse([ 'aa', 'dg', 'bb' ], 'CA', 'ON')).toBe(true);
+    it('should return false if the courses don\'t contain \'dg\' and the location is Ontario', () => {
+      expect(invalidCourse([ 'aa', 'bb' ], 'CA', 'ON')).toBe(false);
+    });
+
+    it('should return false if the courses contain \'dg\' but the location is not Ontario', () => {
+      expect(invalidCourse([ 'aa', 'dg', 'bb' ], 'US', 'ON')).toBe(false);
+      expect(invalidCourse([ 'aa', 'dg', 'bb' ], 'CA', 'BC')).toBe(false);
     });
   });
 });
