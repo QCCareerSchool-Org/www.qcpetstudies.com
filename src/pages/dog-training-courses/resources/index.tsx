2cdfa06ea8fdae5906363fb34821fd4ee2ed4316
Mireille Pitre <mireille@qccareerschool.com>
2021-12-07 14:56:59 -0500

    Change a bunch of links & content

--- src/pages/dog-training-courses/resources/index.tsx
+++ src/pages/dog-training-courses/resources/index.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 import { DefaultLayout } from '../../../components/DefaultLayout';
 import { SEO } from '../../../components/SEO';
-import DogTrainingResourcesBackground from '../../../images/backgrounds/dt-resources.jpg';
+import DogTrainingResourcesBackground from '../../../images/backgrounds/working-at-home-office-with-dog.jpg';
 import DogTrainingCareer from '../../../images/cards/dog-training-career.jpg';
 import LearningInClassOnline from '../../../images/cards/learning-in-class-online.jpg';
 import StudentGrad from '../../../images/cards/student-grad.jpg';
@@ -14,7 +14,7 @@ const DogTrainingResourcesPage: NextPage = () => {
     <DefaultLayout>
       <SEO
         title="Dog Training Resources"
-        description="Learn about how to become a professional dog trainer with QC’s online courses."
+        description="Ues these resources to decide if a career in dog training is right for you!"
         canonical="/dog-training-courses/resources"
       />
 
@@ -42,9 +42,9 @@ const DogTrainingResourcesPage: NextPage = () => {
               <div className="card">
                 <Image className="card-img-top" src={DogTrainingCareer} alt="your dog grooming career" />
                 <div className="card-body">
-                  <h3 className="card-title">Your Dog Training Career</h3>
-                  <p className="card-text">Find out how you can launch a successful full-time or part-time career as a professional dog trainer. Read about salary expectations, time commitment, and more.</p>
-                  <Link href=""><a className="link-primary">Learn More</a></Link>
+                  <h3 className="card-title">Dog Training Course Outline</h3>
+                  <p className="card-text">Read a detailed outline of the online dog training course. Find out what you&apos;ll need to be successful, and how long the course will take you to complete.</p>
+                  <Link href="/dog-training-courses/dog-training/course-outline"><a className="link-primary">Learn More</a></Link>
                 </div>
               </div>
             </div>
@@ -52,9 +52,9 @@ const DogTrainingResourcesPage: NextPage = () => {
               <div className="card">
                 <Image src={StudentGrad} alt="Dog Training Course Outline" className="card-img-top" />
                 <div className="card-body">
-                  <h3 className="card-title">Dog Training Course Outline</h3>
-                  <p className="card-text">Read a detailed outline of the online dog training course. Find out what you&apos;ll need to be successful, and how long the course will take you to complete.</p>
-                  <Link href=""><a className="link-primary">Learn More</a></Link>
+                  <h3 className="card-title">Frequently Asked Questions</h3>
+                  <p className="card-text">Do you have questions about taking the online dog training course? Read this list of FAQs to lear more about your tuition options, how the online course works, and more.</p>
+                  <Link href="/about/faq/"><a className="link-primary">Learn More</a></Link>
                 </div>
               </div>
             </div>
@@ -62,9 +62,9 @@ const DogTrainingResourcesPage: NextPage = () => {
               <div className="card">
                 <Image src={LearningInClassOnline} alt="Learning in Class vs. Online" className="card-img-top" />
                 <div className="card-body">
-                  <h3 className="card-title">Learning in Class vs. Online</h3>
-                  <p className="card-text">Is learning dog training online as effective as learning it in a classroom? It might even be better! Read about the pros and cons of online learning to decide if it&apos;s right for you.</p>
-                  <Link href="/dog-grooming-courses/resources/class-vs-online-dog-grooming-schools"><a className="link-primary">Learn More</a></Link>
+                  <h3 className="card-title">How the Dog Training Course Works</h3>
+                  <p className="card-text">Learning online is much more convenient than learning in the classroom, but is it as effective? It can be! Find out more about how the online Dog Training course works. </p>
+                  <Link href="/dog-training-courses/how-it-works/"><a className="link-primary">Learn More</a></Link>
                 </div>
               </div>
             </div>
@@ -73,10 +73,9 @@ const DogTrainingResourcesPage: NextPage = () => {
             <div className="col-12 col-lg-6 text-start">
               <h3>Additional links</h3>
               <ul>
-                <li><Link href=""><a className="link-primary">Frequently Asked Questions about the Dog Training Course</a></Link></li>
-                <li><Link href=""><a className="link-primary">Information about QC&apos;s 21-day guarantee</a></Link></li>
-                <li><Link href=""><a className="link-primary">Preview the Dog Training Course for free</a></Link></li>
-                <li><Link href=""><a className="link-primary">Quiz: Do you know your calming signals? (blog link)</a></Link></li>
+                <li><Link href="/about/about-qc-pet-studies#guarantee"><a className="link-primary">Information about QC&apos;s 21-day guarantee</a></Link></li>
+                <li><Link href="/dog-training-courses/dog-training/course-preview/"><a className="link-primary">Preview the Dog Training Course for free</a></Link></li>
+                <li><Link href="/2021/07/dog-training-quiz-do-you-know-your-calming-signals/"><a className="link-primary">Quiz: Do you know your calming signals? (blog link)</a></Link></li>
               </ul>
             </div>
           </div>
