bf33e293a54f8f557a59928e3434dee836e8cca4
Dave Welsh <dave@qccareerschool.com>
2021-12-08 18:08:59 -0500

    Add loaders to price section
    Keeps the height of the section constant to avoid scroll errors

--- src/components/PriceSection.tsx
+++ src/components/PriceSection.tsx
@@ -58,35 +58,53 @@ export const PriceSection = ({ courses, doubleGuarantee, variant = 'dark' }: Pro
               <li><FaInfinity className="text-primary me-2" />Lifetime access</li>
             </ul>
           </div>
-          {price && price.courses.length > 0 && (
-            <>
-              <div className="col-10 offset-1 col-sm-8 offset-sm-2 col-md-6 offset-md-0 col-lg-4 mb-4 mb-md-0 d-flex">
-                <div className="card shadow text-center bg-light flex-fill">
-                  <div className="card-body d-flex flex-column justify-content-around py-4">
-                    <h4 className="text-muted mb-3">Installment Plan</h4>
-                    <Bar variant="secondary" />
-                    <p className="mb-3">Get Started for Only<br /><span className="h2 font-family-open-sans">{price?.currency.symbol}{formatPrice(price?.plans.part.deposit)}</span></p>
-                    <p className="mb-3"><span className="lead">Deposit of {price?.currency.symbol}{formatPrice(price?.plans.part.deposit)}</span><br /><small>{price?.plans.part.installments} monthly payments of {price?.currency.symbol}{formatPrice(price?.plans.part.installmentSize)}</small></p>
-                    <p className="mb-0"><a href={enrollLink} className="btn btn-outline-dark">Enroll Now</a></p>
-                  </div>
-                </div>
+          <div className="col-10 offset-1 col-sm-8 offset-sm-2 col-md-6 offset-md-0 col-lg-4 mb-4 mb-md-0 d-flex">
+            <div className="card shadow text-center bg-light flex-fill">
+              <div className="card-body d-flex flex-column justify-content-around py-4">
+                <h4 className="text-muted mb-3">Installment Plan</h4>
+                <Bar variant="secondary" />
+                {price && price.courses.length > 0
+                  ? (
+                    <>
+                      <p className="mb-3">Get Started for Only<br /><span className="h2 font-family-open-sans">{price?.currency.symbol}{formatPrice(price?.plans.part.deposit)}</span></p>
+                      <p className="mb-3"><span className="lead">Deposit of {price?.currency.symbol}{formatPrice(price?.plans.part.deposit)}</span><br /><small>{price?.plans.part.installments} monthly payments of {price?.currency.symbol}{formatPrice(price?.plans.part.installmentSize)}</small></p>
+                      <p className="mb-0"><a href={enrollLink} className="btn btn-outline-dark">Enroll Now</a></p>
+                    </>
+                  )
+                  : <Loader />
+                }
               </div>
-              <div className="col-10 offset-1 col-sm-8 offset-sm-2 col-md-6 offset-md-0 col-lg-4 d-flex">
-                <div className="card shadow text-center bg-white flex-fill">
-                  <div className="card-body d-flex flex-column justify-content-around py-4">
-                    <h4 className="text-muted mb-3">Pay in Full</h4>
-                    <Bar variant="secondary" />
-                    <p className="mb-3">One-Time Payment<br /><span className="h2 font-family-open-sans">{price?.currency.symbol}{formatPrice(price?.plans.full.total)}</span></p>
-                    <p className="mb-3"><span className="lead">SAVE {price?.currency.symbol}{formatPrice(price?.plans.full.discount)}</span><br /><small>When you pay in full</small></p>
-                    <p className="mb-0"><a href={enrollLink} className="btn btn-secondary">Enroll Now</a></p>
-                  </div>
-                </div>
+            </div>
+          </div>
+          <div className="col-10 offset-1 col-sm-8 offset-sm-2 col-md-6 offset-md-0 col-lg-4 d-flex">
+            <div className="card shadow text-center bg-white flex-fill">
+              <div className="card-body d-flex flex-column justify-content-around py-4">
+                <h4 className="text-muted mb-3">Pay in Full</h4>
+                <Bar variant="secondary" />
+                {price && price.courses.length > 0
+                  ? (
+                    <>
+                      <p className="mb-3">One-Time Payment<br /><span className="h2 font-family-open-sans">{price?.currency.symbol}{formatPrice(price?.plans.full.total)}</span></p>
+                      <p className="mb-3"><span className="lead">SAVE {price?.currency.symbol}{formatPrice(price?.plans.full.discount)}</span><br /><small>When you pay in full</small></p>
+                      <p className="mb-0"><a href={enrollLink} className="btn btn-secondary">Enroll Now</a></p>
+                    </>
+                  )
+                  : <Loader />
+                }
               </div>
-            </>
-          )}
+            </div>
+          </div>
         </div>
       </div>
       <GuaranteeModal show={popup} doubleGuarantee={doubleGuarantee} toggle={toggle} />
     </section>
   );
 };
+
+const Loader = (): ReactElement => (
+  <div className="d-flex justify-content-center align-items-center" style={{ height: 186 }}>
+    <div className="spinner-border" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </div>
+  </div>
+);
