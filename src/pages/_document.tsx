ee3ec9448e13371931c4e9609897bb6c6920d590
Dave Welsh <dave@qccareerschool.com>
2022-01-04 11:05:19 -0500

    Update Google Analytics
    Move scripts from _document.tsx to _app.tsx
    Remove uneeded type declaration
    Report web vitals

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,19 +13,6 @@ class MyDocument extends Document {
       <Html lang="en" className="h-100" prefix="og: http://ogp.me/ns#">
         <Head>
           <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{ __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', { page_path: window.location.pathname });
-              gtag('config', 'AW-1071836607');
-            ` }}
-          />
-          <script
             dangerouslySetInnerHTML={{ __html: `
               piAId = '948642';
               piCId = '34913';
