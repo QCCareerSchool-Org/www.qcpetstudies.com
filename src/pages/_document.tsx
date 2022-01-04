7156a5d2c4ee428c6f917c80f48d109775c42f26
Dave Welsh <dave@qccareerschool.com>
2022-01-04 11:28:39 -0500

    Move GA back to document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,6 +2,14 @@
 import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from 'next/document';
 import { ReactElement } from 'react';
 
+const googleAnalyticsScript = `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', { page_path: window.location.pathname });
+gtag('config', 'AW-1071836607');
+`;
+
 class MyDocument extends Document {
   public static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
     const initialProps = await Document.getInitialProps(ctx);
@@ -12,6 +20,11 @@ class MyDocument extends Document {
     return (
       <Html lang="en" className="h-100" prefix="og: http://ogp.me/ns#">
         <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script dangerouslySetInnerHTML={{ __html: googleAnalyticsScript }} />
           <script
             dangerouslySetInnerHTML={{ __html: `
               piAId = '948642';
