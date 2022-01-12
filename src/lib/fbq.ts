03362961f41ee689167a22e7832d08eb99f75515
Dave Welsh <dave@qccareerschool.com>
2022-01-12 13:35:58 -0500

    Add facebook tracking

---
+++ src/lib/fbq.ts
@@ -0,0 +1,26 @@
+type FBQ = {
+  (action: 'track', type: 'PageView' | 'Lead'): void;
+  (action: 'track', type: 'Purchase', params: { value: number; currency: string }): void;
+  (action: 'trackCustom', type: 'VirtualPageView', params: { url: string }): void;
+};
+
+declare global {
+  interface Window {
+    fbq: FBQ;
+  }
+}
+
+// log the page view with a specific URL
+export const fbqPageview = (url: string): void => {
+  window.fbq('trackCustom', 'VirtualPageView', { url });
+};
+
+// log the conversion
+export const fbqLead = (): void => {
+  window.fbq('track', 'Lead');
+};
+
+// log a sale
+export const fbqSale = (value: number, currency: string): void => {
+  window.fbq('track', 'Purchase', { value, currency });
+};
--- src/lib/uet.ts
+++ src/lib/uet.ts
@@ -4,7 +4,7 @@ declare global {
   }
 }
 
-// log the pageview with their URL
+// log the page view with a specific URL
 export const uetPageview = (url: string): void => {
   window.uetq = window.uetq ?? [];
   window.uetq.push('event', 'page_view', { page_path: url }); // eslint-disable-line camelcase
