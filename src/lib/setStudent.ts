23fad98a17400d17d08bd8509b3ba4ddd03ee3e8
Dave Welsh <dave@qccareerschool.com>
2022-01-14 16:47:33 -0500

    Re-add header

--- src/lib/setStudent.ts
+++ src/lib/setStudent.ts
@@ -1,11 +1,13 @@
 import * as HttpStatus from '@qccareerschool/http-status';
 
 export const setStudent = async (enrollmentId: number, code: string): Promise<void> => {
-  const payload = { enrollmentId, code };
   const url = 'https://api.qccareerschool.com/pardot/setStudent';
   const response = await fetch(url, {
     method: 'post',
-    body: JSON.stringify(payload),
+    body: JSON.stringify({ enrollmentId, code }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
   });
   if (!response.ok) {
     throw new HttpStatus.HttpResponse(response.status, response.statusText);
