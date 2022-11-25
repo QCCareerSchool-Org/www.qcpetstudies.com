188e38e3a4e0130a53ece5cf5f9269073eed648f
Dave Welsh <dave@qccareerschool.com>
2022-11-25 10:51:12 -0500

    Update how it works and resources pages

--- src/pages/certification-courses/how-it-works.tsx
+++ src/pages/certification-courses/how-it-works.tsx
@@ -19,8 +19,8 @@ const DogTrainingHowItWorksPage: NextPageWithLayout = () => {
   return (
     <>
       <SEO
-        title="How QC's Online Dog Training Course Works"
-        description="Become a certified dog trainer online. Find out how QC’s online professional dog trainer course works!"
+        title="How QC's Online Courses Work"
+        description="Become a certified dog trainer online. Find out how QC’s online professional courses work!"
         canonical="/certification-courses/how-it-works"
       />
 
@@ -43,10 +43,10 @@ const DogTrainingHowItWorksPage: NextPageWithLayout = () => {
             </div>
             <div className="col-12 col-lg-8 col-xl-9">
               <h2>Learning Online</h2>
-              <p className="lead">Learning to become a dog trainer online can be just as effective as learning in a classroom.</p>
-              <p>QC has been delivering online courses for over 30 years. You'll benefit from this experience in the Online Dog Training Course!</p>
-              <p>The key to successful online training is to provide you with tons of hands-on experience throughout your course. Combine that with detailed individualized feedback from your tutor and you're guaranteed to receive a well-rounded educational experience!</p>
-              <p className="mb-0">Wondering exactly how your dog trainer education will unfold? Here's a detailed overview of how your course, assignments, grades, and graduation will work.</p>
+              <p className="lead">Learning online can be just as effective as learning in a classroom.</p>
+              <p>QC has been delivering online courses for over 30 years. You'll benefit from this experience!</p>
+              <p>The key to successful online training is to provide you with tons of hands-on experience throughout your course. Combine that with detailed individualized feedback from your tutor and you're guaranteed to receive a well-rounded educational experience.</p>
+              <p className="mb-0">Wondering exactly how your education will unfold? Here's a detailed overview of how your course, assignments, grades, and graduation will work.</p>
             </div>
           </div>
         </div>
@@ -56,9 +56,9 @@ const DogTrainingHowItWorksPage: NextPageWithLayout = () => {
         <div className="container text-center">
           <div className="row justify-content-center align-items-center">
             <div className="col-12 col-lg-8 col-xl-9 mb-4 mb-lg-0 text-lg-start">
-              <h2>Starting the <strong>Dog Training Course</strong></h2>
-              <p>After you enroll online, you'll receive login instructions for your online student center within one business day. Follow the instructions to start your dog training journey!</p>
-              <p>Your online student center will contain all your course materials: Your course guides, lesson texts, assignment templates and video tutorials are all in the student center. This is also where you're going to upload your assignments and review your tutor's feedback.</p>
+              <h2>Starting the Course</h2>
+              <p>After you enroll online, you'll receive login instructions for your online student center within one business day. Follow the instructions to start your journey!</p>
+              <p>Your online student center will contain all your course materials: Your course guides, lesson texts, assignment templates and video tutorials are all in the Student Center. This is also where you're going to upload your assignments and review your tutor's audio feedback.</p>
               <p className="mb-0">View a <Link href="/certification-courses/dog-training/course-outline"><a className="link-primary">detailed course outline</a></Link> to learn more about your dog trainer course.</p>
             </div>
             <div className="col-12 col-sm-10 col-md-8 col-lg-4 col-xl-3">
@@ -72,19 +72,19 @@ const DogTrainingHowItWorksPage: NextPageWithLayout = () => {
 
       <section>
         <div className="container text-center">
-          <h2>Your Dog <strong>Training Assignments</strong></h2>
+          <h2>Your Assignments</h2>
           <div className="row justify-content-center">
             <div className="col-12 col-lg-8 mb-4">
-              <p className="mb-0">Throughout the course, you'll complete a variety of theoretical and practical assignments that are designed to test your understanding and develop your training skills.</p>
+              <p className="mb-0">Throughout the course, you'll complete a variety of theoretical and practical assignments that are designed to test your understanding and develop your skills.</p>
             </div>
             <div className="col-12 col-lg-6 d-flex mb-4 mb-lg-0">
               <div className="card w-100">
                 <div className="card-body d-flex flex-column">{/* flex, so that we can align the last element to the bottom */}
                   <Bar variant="secondary" />
                   <h3>Theory-Based Assignments</h3>
-                  <p className="card-text">Being a dog trainer requires a high level of theoretical knowledge of a wide range of topics. You have to become an expert in learning theory, stages of development, dog breed characteristics, training methods, and much more.</p>
-                  <p className="card-text">Through a series of quizzes, short answer assignments and case studies, your knowledge of dog training theory will be put to the test. As you work through your online course, the first few units will focus heavily on theory-based assignments. Your knowledge and skills will improve during this time, and you'll gradually notice a shift toward more and more practical assignments in the later units of the course.</p>
-                  <p className="card-text mt-auto"><a href="/documents/dog-training/C1.pdf" className="link-primary" target="_blank">View Sample Assignment</a></p>
+                  <p className="card-text">Working with pets requires a high level of theoretical knowledge of a wide range of topics. You have to become an expert in learning theory, stages of development, dog breed characteristics, training methods, and much more.</p>
+                  <p className="card-text">Through a series of quizzes, short answer assignments and case studies, your knowledge of theory will be put to the test. As you work through your online course, the first few units will focus heavily on theory-based assignments. Your knowledge and skills will improve during this time, and you'll gradually notice a shift toward more and more practical assignments in the later units of the course.</p>
+                  <p className="card-text mt-auto"><a href="/documents/dog-training/C1.pdf" className="link-primary" target="_blank">View a sample assignment from the Dog Training course</a></p>
                 </div>
               </div>
             </div>
@@ -93,9 +93,9 @@ const DogTrainingHowItWorksPage: NextPageWithLayout = () => {
                 <div className="card-body d-flex flex-column">{/* flex, so that we can align the last element to the bottom */}
                   <Bar variant="secondary" />
                   <h3>Practical Assignments</h3>
-                  <p className="card-text">Once you're familiar with dog training fundamentals, you need to practice your skills on actual live dogs. Luckily in dog training, you can use your own dog(s) to practice methods and skills.</p>
-                  <p className="card-text">At the start of your course, your practical assignments will focus on interpreting behaviors exhibited by dogs in your household and environment. As you progress through your training, your practical assignments will evolve to teaching dogs new behaviors using different methods and techniques, and modifying common unwanted behaviors. You'll take videos of you working with dogs so that your tutor can evaluate your work!</p>
-                  <p className="card-text mt-auto"><a href="/documents/dog-training/G6.pdf" className="link-primary" target="_blank">View Sample Assignment</a></p>
+                  <p className="card-text">Once you're familiar with fundamentals, you need to practice your skills on actual dogs. Luckily, you can use your own dog(s) to practice methods and skills.</p>
+                  <p className="card-text">At the start of your course, your practical assignments will focus on interpreting behaviors exhibited by dogs in your household and environment. As you progress through your training, your practical assignments will evolve. You'll take videos of you working with dogs so that your tutor can evaluate your work!</p>
+                  <p className="card-text mt-auto"><a href="/documents/dog-training/G6.pdf" className="link-primary" target="_blank">View a sample assignment from the Dog Training course</a></p>
                 </div>
               </div>
             </div>
@@ -110,7 +110,7 @@ const DogTrainingHowItWorksPage: NextPageWithLayout = () => {
               <div className="section pe-lg-5">
                 <h2>Receiving Feedback From <strong>Your Tutor</strong></h2>
                 <p className="lead">You'll receive advice and detailed feedback from your tutor after every unit you complete.</p>
-                <p>Your tutor truly cares about your success and will encourage you to submit your very best work. After each unit of the dog training course, you'll receive an audio file where your tutor explains what you did well, and where you should focus to improve your skills even further. See how your skills and confidence improve as you work your way through the course!</p>
+                <p>Your tutor truly cares about your success and will encourage you to submit your very best work. After each unit, you'll receive an audio file where your tutor explains what you did well, and where you should focus to improve your skills even further. See how your skills and confidence improve as you work your way through the course!</p>
                 <p className="mb-0"><Link href="/certification-courses/dog-training#tutors"><a className="btn btn-outline-secondary">Meet Your Tutor</a></Link></p>
               </div>
             </div>
@@ -131,7 +131,7 @@ const DogTrainingHowItWorksPage: NextPageWithLayout = () => {
                 <Image src={GraduationCertificateImage} alt="IDTP™ Certification logo" height="180" width="180" />
               </div>
               <h2>Your <strong>Graduation</strong></h2>
-              <p>Once you've completed your dog training course and your tuition has been paid in full, you'll be all set to graduate. Your professional dog trainer certificate will be sent straight to your home!</p>
+              <p>Once you've completed your course and your tuition has been paid in full, you'll be all set to graduate. Your professional certificate will be sent straight to your home!</p>
               <p className="lead mb-0">Please keep in touch. We'd love to hear how you're doing!</p>
             </div>
           </div>
--- src/pages/certification-courses/resources.tsx
+++ src/pages/certification-courses/resources.tsx
@@ -4,9 +4,9 @@ import Link from 'next/link';
 
 import { SEO } from '../../components/SEO';
 import DogTrainingResourcesBackground from '../../images/backgrounds/working-at-home-office-with-dog.jpg';
-import DogTrainingCareer from '../../images/cards/dog-training-career.jpg';
 import LearningInClassOnline from '../../images/cards/learning-in-class-online.jpg';
 import StudentGrad from '../../images/cards/student-grad.jpg';
+import VirtualClassroom from '../../images/cards/virtual-classroom.jpg';
 
 const LearningResourcesPage: NextPage = () => (
   <>
@@ -38,17 +38,17 @@ const LearningResourcesPage: NextPage = () => (
         <div className="row justify-content-center mb-5">
           <div className="col-12 col-sm-8 col-lg-4 mb-4 mb-lg-0 d-flex">
             <div className="card">
-              <Image className="card-img-top" src={DogTrainingCareer} alt="your dog grooming career" />
+              <Image src={VirtualClassroom} className="card-img-top" alt="smiling lady at a laptop" />
               <div className="card-body">
-                <h3 className="card-title">Dog Training Course Outline</h3>
-                <p className="card-text">Read a detailed outline of the online dog training course. Find out what you'll need to be successful, and how long the course will take you to complete.</p>
-                <Link href="/certification-courses/dog-training/course-outline"><a className="link-primary">Learn More</a></Link>
+                <h3 className="card-title">Virtual Classroom</h3>
+                <p className="card-text">The Virtual Classroom is a private Facebook group exclusively for QC Pet Studies&apos; students. You&apos;ll have the opportunity to ask course-related questions, interact with your Teaching Assistant, access supplementary content and build a sense of community with other aspiring pet-industry professionals.</p>
+                <a href="https://www.facebook.com/groups/qcpetstudiesvirtualclassroom" className="link-primary" target="_blank" rel="noreferrer">Learn More</a>
               </div>
             </div>
           </div>
           <div className="col-12 col-sm-8 col-lg-4 mb-4 mb-lg-0 d-flex">
             <div className="card">
-              <Image src={StudentGrad} alt="Dog Training Course Outline" className="card-img-top" />
+              <Image src={StudentGrad} className="card-img-top" alt="smiling man crouched down, petting a dog" />
               <div className="card-body">
                 <h3 className="card-title">Frequently Asked Questions</h3>
                 <p className="card-text">Do you have questions about taking the online dog training course? Read this list of FAQs to learn more about your tuition options, how the online course works, and more.</p>
@@ -58,22 +58,25 @@ const LearningResourcesPage: NextPage = () => (
           </div>
           <div className="col-12 col-sm-8 col-lg-4 d-flex">
             <div className="card">
-              <Image src={LearningInClassOnline} alt="Learning in Class vs. Online" className="card-img-top" />
+              <Image src={LearningInClassOnline} className="card-img-top" alt="dog resting his chin on a laptop" />
               <div className="card-body">
-                <h3 className="card-title">How the Dog Training Course Works</h3>
-                <p className="card-text">Learning online is much more convenient than learning in the classroom, but is it as effective? It can be! Find out more about how the online Dog Training course works. </p>
-                <Link href="/certification-courses/how-it-works/"><a className="link-primary">Learn More</a></Link>
+                <h3 className="card-title">How It Works</h3>
+                <p className="card-text">Learning online is much more convenient than learning in the classroom, but is it as effective? It can be! Find out more about how QC&apos;s online courses work. </p>
+                <Link href="/certification-courses/how-it-works"><a className="link-primary">Learn More</a></Link>
               </div>
             </div>
           </div>
         </div>
         <div className="row justify-content-center">
-          <div className="col-12 col-lg-6 text-start">
+          <div className="col-12 text-start">
             <h3>Additional links</h3>
             <ul>
               <li><Link href="/about/about-qc-pet-studies#guarantee"><a className="link-primary">Information about QC's 21-day guarantee</a></Link></li>
-              <li><Link href="/certification-courses/dog-training/course-preview/"><a className="link-primary">Preview the Dog Training Course for free</a></Link></li>
-              <li><Link href="/2021/07/dog-training-quiz-do-you-know-your-calming-signals/"><a className="link-primary">Quiz: Do you know your calming signals? (blog link)</a></Link></li>
+              <li><Link href="/certification-courses/dog-training/course-preview"><a className="link-primary">Preview the Dog Training Course for free</a></Link></li>
+              <li><Link href="/certification-courses/dog-grooming/course-outline"><a className="link-primary">Dog Grooming Course Outline</a></Link></li>
+              <li><Link href="/certification-courses/dog-training/course-outline"><a className="link-primary">Dog Training Course Outline</a></Link></li>
+              <li><Link href="/certification-courses/dog-daycare/course-outline"><a className="link-primary">Dog Daycare Course Outline</a></Link></li>
+              <li><Link href="/2021/07/dog-training-quiz-do-you-know-your-calming-signals"><a className="link-primary">Quiz: Do you know your calming signals? (blog link)</a></Link></li>
             </ul>
           </div>
         </div>
