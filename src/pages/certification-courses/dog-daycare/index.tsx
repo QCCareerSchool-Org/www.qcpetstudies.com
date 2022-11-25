d0fd5af2f49a0302b26a02d6b54de654a1d026d2
Dave Welsh <dave@qccareerschool.com>
2022-11-25 10:18:57 -0500

    Add images to DD overview page, outline section

--- src/pages/certification-courses/dog-daycare/index.tsx
+++ src/pages/certification-courses/dog-daycare/index.tsx
@@ -17,6 +17,10 @@ import DogCareBackground from '../../../images/backgrounds/jack-russel-terrier-i
 import GuaranteeIcon from '../../../images/course-overview-icons/guarantee.svg';
 import OutlineIcon from '../../../images/course-overview-icons/outline.svg';
 import TutorIcon from '../../../images/course-overview-icons/tutor.svg';
+import IntroductoryUnitsImage from '../../../images/dd-outline-part-1.jpg';
+import ActivitiesImage from '../../../images/dd-outline-part-2.jpg';
+import ServicesImage from '../../../images/dd-outline-part-3.jpg';
+import DogCareBusinessImage from '../../../images/dd-outline-part-4.jpg';
 import CertificationGoldImage from '../../../images/IDCP-certification-gold.svg';
 import { formatPrice } from '../../../lib/formatPrice';
 import { getLocation } from '../../../lib/getLocation';
@@ -40,6 +44,7 @@ const DogCarePage: NextPageWithLayout<Props> = ({ price }) => {
 
   const mdOrGreater = screenWidth >= 768;
   const lgOrGreater = screenWidth >= 992;
+  const xlOrGreater = screenWidth >= 1200;
 
   const md = mdOrGreater && !lgOrGreater;
 
@@ -58,7 +63,7 @@ const DogCarePage: NextPageWithLayout<Props> = ({ price }) => {
             <div className="mb-4">
               <Image src={CertificationGoldImage} alt="International Dog Grooming Professional IDGP certification" height="125" width="125" />
             </div>
-            <h1>Dog Care</h1>
+            <h1>Dog Daycare</h1>
             {price && price.plans.part.deposit > 0 && <h4>Get Started for Only <strong>{price.currency.symbol}{formatPrice(price.plans.part.deposit)}</strong></h4>}
             <p><em><a href="#tuition" className="text-white">See tuition details</a></em></p>
             <a href="https://enroll.qcpetstudies.com?c[]=dd"><button className="btn btn-secondary btn-lg">Enroll Online</button></a>
@@ -135,27 +140,47 @@ const DogCarePage: NextPageWithLayout<Props> = ({ price }) => {
           <Accordion defaultActiveKey="0">
             <AccordionToggle title="Part 1: Introductory Units" eventKey="0" />
             <AccordionSection eventKey="0">
-              <p>In the first part of your course, you&apos;ll build a foundation for your career in the pet industry. You&apos;ll discover the many opportunities available to you as a dog daycare provider. You might open a doggy daycare facility, offer dog-walking services or board dogs over night. You&apos;ll also learn all about the tools and supplies you&apos;ll need to safely provide these services. Of course, a strong understanding of the dogs themselves will be essential to your career as a dog daycare provider. In the introductory units of your course, you&apos;ll learn all about dog behavior and communication. You&apos;ll discover how a dog&apos;s breed, age and health can impact its behavior.</p>
-              <p>With all this information, you&apos;ll feel confident and prepared when you care for dogs.</p>
-              <p className="mb-0"><Link href="/certification-courses/dog-daycare/course-outline"><a className="link-primary">View a more-detailed course syllabus</a></Link></p>
+              <div className="row">
+                <div className="col-12 col-xl-8">
+                  <p>In the first part of your course, you&apos;ll build a foundation for your career in the pet industry. You&apos;ll discover the many opportunities available to you as a dog daycare provider. You might open a doggy daycare facility, offer dog-walking services or board dogs over night. You&apos;ll also learn all about the tools and supplies you&apos;ll need to safely provide these services. Of course, a strong understanding of the dogs themselves will be essential to your career as a dog daycare provider. In the introductory units of your course, you&apos;ll learn all about dog behavior and communication. You&apos;ll discover how a dog&apos;s breed, age and health can impact its behavior.</p>
+                  <p>With all this information, you&apos;ll feel confident and prepared when you care for dogs.</p>
+                  <p className="mb-0"><Link href="/certification-courses/dog-daycare/course-outline"><a className="link-primary">View a more-detailed course syllabus</a></Link></p>
+                </div>
+                {xlOrGreater && <div className="col-4"><Image src={IntroductoryUnitsImage} alt="two dogs sniffing each other in a park" /></div>}
+              </div>
             </AccordionSection>
             <AccordionToggle title="Part 2: Activities with Dogs" eventKey="1" />
             <AccordionSection eventKey="1">
-              <p>Although caring for dogs is a big responsibility, it&apos;s also a lot of fun! You&apos;ll learn all about the activities you can perform with dogs to ensure they receive social, physical, emotional and cognitive stimulation at daycare. You&apos;ll study essential topics, from collection and drop-off services to the best locations to exercise dogs. You&apos;ll also learn how to plan fun activities that will engage your furry friends!</p>
-              <p>At QC Pet Studies, the safety of dogs is of the utmost importance. You&apos;ll study the essential precautions you&apos;ll take to keep dogs safe. You&apos;ll be well prepared to provide every dog with an exciting, stimulating and safe experience as a dog daycare provider.</p>
-              <p>Your clients will rest assured knowing their beloved pets will be returned happy and safe at the end of their stay with you.</p>
-              <p className="mb-0"><Link href="/certification-courses/dog-daycare/course-outline"><a className="link-primary">View a more-detailed course syllabus</a></Link></p>
+              <div className="row">
+                <div className="col-12 col-xl-8">
+                  <p>Although caring for dogs is a big responsibility, it&apos;s also a lot of fun! You&apos;ll learn all about the activities you can perform with dogs to ensure they receive social, physical, emotional and cognitive stimulation at daycare. You&apos;ll study essential topics, from collection and drop-off services to the best locations to exercise dogs. You&apos;ll also learn how to plan fun activities that will engage your furry friends!</p>
+                  <p>At QC Pet Studies, the safety of dogs is of the utmost importance. You&apos;ll study the essential precautions you&apos;ll take to keep dogs safe. You&apos;ll be well prepared to provide every dog with an exciting, stimulating and safe experience as a dog daycare provider.</p>
+                  <p>Your clients will rest assured knowing their beloved pets will be returned happy and safe at the end of their stay with you.</p>
+                  <p className="mb-0"><Link href="/certification-courses/dog-daycare/course-outline"><a className="link-primary">View a more-detailed course syllabus</a></Link></p>
+                </div>
+                {xlOrGreater && <div className="col-4"><Image src={ActivitiesImage} alt="lady walking three dogs" /></div>}
+              </div>
             </AccordionSection>
             <AccordionToggle title="Part 3: Developing Your Services" eventKey="2" />
             <AccordionSection eventKey="2">
-              <p>As a dog-care provider, you can offer a huge range of valuable services to pet owners. In this part of your course, you&apos;ll learn how to develop standard operating procedures to ensure that you are always providing the best care possible for your furry friends. With detailed procedures in place, you&apos;ll be prepared to handle any situation you might encounter. Every detail is important when you&apos;re caring for someone&apos;s pet. You&apos;ll also discover how you can enrich the dog daycare experience by offering supplementary services, including transportation, training or grooming.</p>
-              <p>You&apos;ll stand out as the best dog daycare in town if dog owners know you can take care of all of their pets&apos; needs.</p>
-              <p className="mb-0"><Link href="/certification-courses/dog-daycare/course-outline"><a className="link-primary">View a more-detailed course syllabus</a></Link></p>
+              <div className="row">
+                <div className="col-12 col-xl-8">
+                  <p>As a dog-care provider, you can offer a huge range of valuable services to pet owners. In this part of your course, you&apos;ll learn how to develop standard operating procedures to ensure that you are always providing the best care possible for your furry friends. With detailed procedures in place, you&apos;ll be prepared to handle any situation you might encounter. Every detail is important when you&apos;re caring for someone&apos;s pet. You&apos;ll also discover how you can enrich the dog daycare experience by offering supplementary services, including transportation, training or grooming.</p>
+                  <p>You&apos;ll stand out as the best dog daycare in town if dog owners know you can take care of all of their pets&apos; needs.</p>
+                  <p className="mb-0"><Link href="/certification-courses/dog-daycare/course-outline"><a className="link-primary">View a more-detailed course syllabus</a></Link></p>
+                </div>
+                {xlOrGreater && <div className="col-4"><Image src={ServicesImage} alt="lady removing a dog's collar at the beach" /></div>}
+              </div>
             </AccordionSection>
             <AccordionToggle title="Part 4: Dog Daycare Business Training" eventKey="3" />
             <AccordionSection eventKey="3">
-              <p>Once you&apos;ve learned and practiced the skills you&apos;ll need to take care of dogs, you&apos;ll focus on developing your dog daycare business. In this optional part of your course, you&apos;ll learn how to turn your passion for working with dogs into a thriving career. You&apos;ll discover what it takes to start a business, from naming and registering your business, to selling your services. Honing these important business skills will help you to jumpstart your career and launch a successful business.</p>
-              <p className="mb-0"><Link href="/certification-courses/dog-daycare/course-outline"><a className="link-primary">View a more-detailed course syllabus</a></Link></p>
+              <div className="row">
+                <div className="col-12 col-xl-8">
+                  <p>Once you&apos;ve learned and practiced the skills you&apos;ll need to take care of dogs, you&apos;ll focus on developing your dog daycare business. In this optional part of your course, you&apos;ll learn how to turn your passion for working with dogs into a thriving career. You&apos;ll discover what it takes to start a business, from naming and registering your business, to selling your services. Honing these important business skills will help you to jumpstart your career and launch a successful business.</p>
+                  <p className="mb-0"><Link href="/certification-courses/dog-daycare/course-outline"><a className="link-primary">View a more-detailed course syllabus</a></Link></p>
+                </div>
+                {xlOrGreater && <div className="col-4"><Image src={DogCareBusinessImage} alt="man writing at a desk with a Jack Russel Terrier on his lap" /></div>}
+              </div>
             </AccordionSection>
           </Accordion>
         </div>
