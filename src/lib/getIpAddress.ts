de0ae496bb43860611ac9a17f585a9107e338a76
Dave Welsh <dave@qccareerschool.com>
2023-02-16 15:32:33 -0500

    Fix bug in ip address calculation

--- src/lib/getIpAddress.ts
+++ src/lib/getIpAddress.ts
@@ -1,5 +1,5 @@
 import { GetServerSidePropsContext } from 'next';
 
 export const getIpAddress = (context: GetServerSidePropsContext): string | undefined => {
-  return Array.isArray(context.req.headers['x-real-ip']) ? context.req.headers['x-real-ip'][0] : context.req.headers['x-real-ip'] ?? context.req.socket.remoteAddress;
+  return (Array.isArray(context.req.headers['x-real-ip']) ? context.req.headers['x-real-ip'][0] : context.req.headers['x-real-ip']) ?? context.req.socket.remoteAddress;
 };
