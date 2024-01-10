792c4b4c8cb6aec91612b772032680499dbdda5c
Dave Welsh <dave@qccareerschool.com>
2024-01-10 17:17:06 -0500

    Fixed linting error

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -162,7 +162,7 @@ const HomePage: NextPageWithLayout = () => {
                   />
                 </div>
                 <h3><span className="badge rounded-pill bg-secondary">New</span> Dog Behavior | <em className="gold">IDBS&trade;</em></h3>
-                <p>Once you have successfully completed QC's Dog Training course you can advance your education with the Dog Behavior course. You’ll build on your excellent foundation in dog training and learn how to deal with advanced concerns like anxiety, phobias, reactivity, and even aggression. Learn specialized behavior modification skills so you'll be ready to help any dog that comes your way.</p>
+                <p>Once you have successfully completed QC's Dog Training course you can advance your education with the Dog Behavior course. You'll build on your excellent foundation in dog training and learn how to deal with advanced concerns like anxiety, phobias, reactivity, and even aggression. Learn specialized behavior modification skills so you'll be ready to help any dog that comes your way.</p>
               </div>
               <Link href="/certification-courses/dog-training"><button className="btn btn-navy">Course Overview</button></Link>
             </CertificationCard>
