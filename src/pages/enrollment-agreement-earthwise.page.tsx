f6c6868aaf262eb15b0c1942ac7c248b3aa9ec35
Dave Welsh <dave@qccareerschool.com>
2024-02-28 12:16:14 -0500

    Updated agreement

--- src/pages/enrollment-agreement-earthwise.page.tsx
+++ src/pages/enrollment-agreement-earthwise.page.tsx
@@ -21,25 +21,33 @@ const AgreementPage: NextPageWithLayout = () => {
       />
 
       <h1>Enrollment Agreement</h1>
+
       <h2>1. Admission Eligibility</h2>
-      <p>EarthWise franchise owners are eligible to take the &quot;EarthWise Accelerated Grooming Course&quot;. Alternative course options are available for other student groups. Please contact the school at 1-833-600-3751 for more information.</p>
+      <p>EarthWise franchise owners are eligible to take the &ldquo;EarthWise Accelerated Grooming Course&rdquo; owned by QC Quality of Course Inc. (&ldquo;the School&rdquo;). Individuals associated with the management of EarthWise franchise stores may also be eligible to take the &ldquo;EarthWise Accelerated Grooming Course&rdquo; according to the School's discretion. Alternative course options are available for other student groups. Please contact the School at 1-833-600-3751 for more information.</p>
       <h2>2. Course Deadline</h2>
-      <p>Students have 12 weeks from the date of enrollment to complete the &quot;EarthWise Accelerated Grooming Course&quot;. QC is happy to offer an extension to students who need more time. Simply contact the School to arrange an extension before the 12-week deadline. A $50 USD administration fee will be charged.</p>
-      <h2>3. Single User Access</h2>
-      <p>The &quot;EarthWise Accelerated Grooming Course&quot; is designed to be completed by one user only. Login details and course materials are not to be viewed by or distributed to other users or entities.</p>
-      <h2>4. Refund Policy</h2>
-      <p>The EarthWise Accelerated Grooming Course is non-refundable and non-transferable.</p>
-      <h2>5. Code of Conduct</h2>
+      <p>Students have 12 weeks from the date of enrollment to complete the &ldquo;EarthWise Accelerated Grooming Course&rdquo;. QC is happy to offer an extension to students who need more time. Students will simply contact the School to arrange an extension before the 12-week deadline. The student will be responsible for paying a $100 USD administration fee to the School to extend the course by eight weeks.</p>
+      <h2>3. Final Test &amp; Grading</h2>
+      <p>There is no deadline for completing course lessons or submitting the final test provided the course is completed within the 12-week course deadline.</p>
+      <p>A grade of B or higher is required to pass the final test and graduate from the course. Failing the final test will require students to redo the test until a passing grade has been achieved. There is currently no charge to redo the final test.</p>
+      <p>Upon successfully completing the final test, students will automatically graduate from the &ldquo;EarthWise Accelerated Grooming Course&rdquo;. Students will be required to complete an application form to receive a certificate of completion.</p>
+      <h2>4. Single User Access and Copyright</h2>
+      <p>The &ldquo;EarthWise Accelerated Grooming Course&rdquo; is designed to be completed by one user only. Similarly, the course auditor role should belong to one user only. Login details and course materials are not to be viewed by or distributed to other users or entities.</p>
+      <p>Copyright for the &ldquo;EarthWise Accelerated Grooming Course&rdquo; is wholly owned by 7904215 Canada Inc. No parts of this course may be reproduced without written permission from 7904215 Canada Inc.</p>
+      <h2>5. Errors and Omissions Policies</h2>
+      <p>QC Quality of Course Inc. has used its best efforts in collecting and preparing material for inclusion in this course. It does not assume and hereby disclaims any liability to any party for any loss or damages caused by errors or omissions in the course, whether such errors or omissions result from negligence, accident, or any other cause.</p>
+      <h2>6. Refund Policy</h2>
+      <p>The &ldquo;EarthWise Accelerated Grooming Course&rdquo; is non-refundable and non-transferable.</p>
+      <h2>7. Code of Conduct</h2>
       <p>The staff at QC are dedicated to providing a top-quality experience to all students. When you communicate with the QC Team, you will be greeted in a courteous, friendly and respectful manner.</p>
       <p>Students who exhibit abusive, threatening or offensive behavior when communicating with any member of the QC team may be placed on academic probation or expelled from the School without a tuition refund.</p>
       <p>QC considers cyber-bullying to be a serious and dangerous situation for all. Any individual who is identifiable as a QC student who is found to exhibit threatening, harassing, abusive or offensive behavior online may be placed on academic probation or expelled from the School without a tuition refund.</p>
-      <h2>6. Cheating &amp; Plagiarism</h2>
-      <p>As a student of QC, you are expected to put in your best effort to complete your course work. Cheating, plagiarism, tampering, or falsification of your work in any way is not permitted.</p>
-      <h2>7. Governing Law and Jurisdiction</h2>
+      <h3>Cheating &amp; Plagiarism</h3>
+      <p>Students are expected to put in their best effort to complete their coursework. Cheating, plagiarism, tampering, or falsification of work in any way is not permitted.</p>
+      <h2>8. Governing Law and Jurisdiction</h2>
       <p>This Agreement is governed by and is to be construed in accordance with the laws of the Province of Ontario and the laws of Canada applicable therein and treated in all respects as an Ontario contract. The parties to this Agreement hereby irrevocably and unconditionally attorn to the exclusive jurisdiction of the courts of the Province of Ontario and all courts competent to hear appeals therefrom.</p>
-      <h2>8. Age of Majority</h2>
+      <h2>9. Age of Majority</h2>
       <p>If you are under 18 or under the age of majority in your state/province, you will need a parental consent form in order to enroll with QC. Please contact the School for further details.</p>
-      <h2>9. Contact Information</h2>
+      <h2>10. Contact Information</h2>
       <p>QC Quality of Course Inc.<br />Corporation Number: 208502-0<br />Business Number (BN): 105927339RC0001</p>
       <p>Email: {emailAddress}<br />Phone: 1-613-749-8248</p>
       <h3>Shipping</h3>
