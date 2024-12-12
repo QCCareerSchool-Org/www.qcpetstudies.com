433a3b3c715d81a5a62cee62872d9d1b52400ec3
Dave Welsh <dave@qccareerschool.com>
2024-12-12 09:30:09 -0500

    Added crawl-delay

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -8,6 +8,7 @@ export default function robots(): MetadataRoute.Robots {
       disallow: [
         '/*?*playAgain=',
       ],
+      crawlDelay: 10,
     },
     sitemap: 'https://www.qcpetstudies.com/sitemap_index.xml',
   };
