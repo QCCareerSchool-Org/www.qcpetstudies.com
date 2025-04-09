ee03b04511ef26987bffced1f194b5ef20227dc4
Dave Welsh <dave@qccareerschool.com>
2025-04-09 10:39:53 -0400

    commented out code

--- src/app/merit/fetchData.ts
+++ src/app/merit/fetchData.ts
@@ -1,18 +1,18 @@
 import type { Submission } from './submission';
-import { isSubmission } from './submission';
+// import { isSubmission } from './submission';
 
 export const fetchData = async (submissionId: string): Promise<Submission> => {
-  console.log('fetching');
+  console.log('fetching', submissionId);
   return { submissionId: 'skdjfh', grade: 'A', name: 'John Doe' };
 
-  const url = `https://studentcenter.qccareerschool.com/api/submissions?submissionId=${encodeURIComponent(submissionId)}`;
-  const response = await fetch(url);
-  if (!response.ok) {
-    throw Error(response.statusText);
-  }
-  const body: unknown = await response.json();
-  if (!isSubmission(body)) {
-    throw Error('Unexpected response');
-  }
-  return body;
+  // const url = `https://studentcenter.qccareerschool.com/api/submissions?submissionId=${encodeURIComponent(submissionId)}`;
+  // const response = await fetch(url);
+  // if (!response.ok) {
+  //   throw Error(response.statusText);
+  // }
+  // const body = await response.json();
+  // if (!isSubmission(body)) {
+  //   throw Error('Unexpected response');
+  // }
+  // return body;
 };
