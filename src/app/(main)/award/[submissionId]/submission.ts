e5aba9c04cd135362663e3052ecde00a37784876
Dave Welsh <dave@qccareerschool.com>
2025-04-23 14:36:38 -0400

    Added unit letter

--- src/app/(main)/award/[submissionId]/submission.ts
+++ src/app/(main)/award/[submissionId]/submission.ts
@@ -5,7 +5,9 @@ export type RawAward = {
   submissionId: string;
   courseName: string;
   schoolName: School;
+  unitLetter: string;
   grade: string;
+  /** student name */
   name: string;
   /** string date */
   created: string;
@@ -15,8 +17,11 @@ export type Award = {
   submissionId: string;
   courseName: string;
   schoolName: School;
+  unitLetter: string;
   grade: string;
+  /** student name */
   name: string;
+  /** string date */
   created: Date;
 };
 
@@ -25,7 +30,8 @@ export const isRawAward = (obj: unknown): obj is RawAward => {
     'submissionId' in obj && typeof obj.submissionId === 'string' &&
     'courseName' in obj && typeof obj.courseName === 'string' &&
     'schoolName' in obj && typeof obj.schoolName === 'string' && isSchool(obj.schoolName) &&
-    'name' in obj && typeof obj.name === 'string' &&
+    'unitLetter' in obj && typeof obj.unitLetter === 'string' &&
     'grade' in obj && typeof obj.grade === 'string' &&
+    'name' in obj && typeof obj.name === 'string' &&
     'created' in obj && typeof obj.created === 'string';
 };
