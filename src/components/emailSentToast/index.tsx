c4965ea6f6a9947188099ac71d910e4d9c505679
Dave Welsh <dave@qccareerschool.com>
2025-09-30 14:11:15 -0400

    Added emphasis about junk folder

--- src/components/emailSentToast/index.tsx
+++ src/components/emailSentToast/index.tsx
@@ -14,7 +14,7 @@ export const EmailSentToast: FC<Props> = ({ emailAddress, firstName }) => {
   useEffect(() => {
     const message = (
       <div className="mx-1">
-        Your email was sent to <strong>{emailAddress}</strong>. If you don't find it in your inbox, please check your junk mail folder.
+        Your email was sent to <strong>{emailAddress}</strong>. If you don't find it in your inbox, please <strong className="text-danger">check your junk mail folder</strong>.
       </div>
     );
     toast.success(message, { delay: 1500, autoClose: 12_000, className: styles.toast });
