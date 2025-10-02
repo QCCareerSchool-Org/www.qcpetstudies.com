36cd103d7515e59b78b8af64a954037ee324e966
Maryam Hassan <maryam.hassan@qccareerschool.com>
2025-10-02 09:19:01 -0400

    Feat: Make 'Book a Call' link open in new tab

--- src/components/supportSection/index.tsx
+++ src/components/supportSection/index.tsx
@@ -23,7 +23,7 @@ export const SupportSection: FC<Props> = ({ showLink }) => (
           <p className="lead mb-0">QC's dedicated team is here to help you select the right course, navigate your materials, and stay on track to reach your career goals. Available seven days a week by phone, email, or live chat, we are here to ensure you get the support you need&mdash;whether it's advice, troubleshooting, or planning your next step.</p>
           <div className="d-flex gap-3 flex-wrap mt-3">
             {showLink && <Link href="/contact-us" className="btn btn-secondary mt-4"><ChatIcon height="22" className="me-2" style={{ position: 'relative', top: -2 }} />Talk to Us Today</Link>}
-            {showLink && <Link href="https://calendly.com/qccareerschool/pet" className="btn btn-outline-light mt-4" style={{ borderWidth: '2px' }}><CalIcon height="22" className="me-2" style={{ position: 'relative', top: -3 }} />Book A Call</Link>}
+            {showLink && <Link href="https://calendly.com/qccareerschool/pet" target="_blank" rel="noopener noreferrer" className="btn btn-outline-light mt-4" style={{ borderWidth: '2px' }}><CalIcon height="22" className="me-2" style={{ position: 'relative', top: -3 }} />Book A Call</Link>}
           </div>
         </div>
       </div>
