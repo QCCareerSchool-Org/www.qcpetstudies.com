4bea814972f2243cc25d8510d08699cd235e1554
Dave Welsh <dave@qccareerschool.com>
2025-11-12 17:02:13 -0500

    Added comment

--- src/withSuspense.tsx
+++ src/withSuspense.tsx
@@ -1,6 +1,7 @@
 import type { FC, ReactNode } from 'react';
 import { Suspense } from 'react';
 
+/** HOC to automatically wrap a component in a Suspense boundary */
 export const withSuspense = <P extends object>(Component: FC<P>, fallback?: ReactNode): FC<P> => {
   const Inner: FC<P> = props => (
     <Suspense fallback={fallback}>
