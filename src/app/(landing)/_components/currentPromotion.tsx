7845356e1afc8c4d682a138aa7d9518abd108bd8
Dave Welsh <dave@qccareerschool.com>
2025-12-01 12:45:42 -0500

    Abstracted banner and switched to Cyber Monday

--- src/app/(landing)/_components/currentPromotion.tsx
+++ src/app/(landing)/_components/currentPromotion.tsx
@@ -1,15 +1,14 @@
 import type { FC } from 'react';
 
-import { BlackFriday2025 } from '@/components/promos/blackFriday2025';
+import { CyberMonday2025 } from '@/components/promos/cyberMonday2025';
 
 interface Props {
   date: number;
   countryCode: string;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const CurrentPromotion: FC<Props> = ({ date, countryCode }) => {
+export const CurrentPromotion: FC<Props> = ({ date }) => {
   if (date >= Date.UTC(2025, 10, 17, 15) && date < Date.UTC(2025, 10, 29, 8)) { // 2025-11-17T10:00 (15:00 UTC) to 2025-11-29T03:00 (08:00 UTC)
-    return <BlackFriday2025 />;
+    return <CyberMonday2025 />;
   }
 };
--- src/components/siteLayout/header.tsx
+++ src/components/siteLayout/header.tsx
@@ -4,17 +4,39 @@ import styles from './header.module.scss';
 import { SecondaryNav } from './secondaryNav';
 import { CountDownTimer } from '@/components/countDownTimer';
 import { MainNav } from '@/components/siteLayout/mainNav';
+import { gbpCountry } from '@/lib/currencies';
 
 interface Props {
   date: number;
   countryCode: string;
 }
 
+const bannerStartDate = Date.UTC(2025, 11, 1, 5); // 2025-12-01T00:00 (05:00 UTC)
+const bannerCountdownStartDate = Date.UTC(2025, 11, 5, 8); // 2025-12-05T03:00 (08:00 UTC)
+const bannerEndDate = Date.UTC(2025, 11, 6, 8); // 2025-12-06T03:00 (8:00 UTC)
+
 export const Header: FC<Props> = ({ date, countryCode }) => {
+  const discount = gbpCountry(countryCode) ? '£400' : '$400';
+
   return (
     <div className={`${styles.wrapper} shadow-lg`}>
       {/* {countryCode === 'CA' && <CanadaHeader />} */}
-      <CountDownTimer date={date} countryCode={countryCode} />
+      <CountDownTimer
+        date={date}
+        startDate={bannerStartDate}
+        countdownStartDate={bannerCountdownStartDate}
+        endDate={bannerEndDate}
+        message={(
+          <span style={{ textTransform: 'uppercase' }}>
+            <span className="d-none d-lg-inline">Don't Miss Out—</span>Cyber Monday Savings Have Arrived! Get {discount} Off Any Course!<br className="d-lg-none" /><button className="btn btn-danger my-2 btn-sm ms-3 text-uppercase">Claim Offer</button>
+          </span>
+        )}
+        lastChanceMessage={(
+          <span style={{ textTransform: 'uppercase' }}>
+            <span className="d-none d-lg-inline">Don't Miss Out—</span>Cyber Monday Savings Have Arrived! Get {discount} Off Any Course!<br /><button className="btn btn-danger my-2 btn-sm ms-3 text-uppercase">Claim Offer</button>
+          </span>
+        )}
+      />
       <MainNav countryCode={countryCode} />
       <SecondaryNav />
     </div>
