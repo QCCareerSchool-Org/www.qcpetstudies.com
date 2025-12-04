11ecc02ad2a345a67a42e58e96e5d93a9edd37f7
Dave Welsh <dave@qccareerschool.com>
2025-12-04 12:46:39 -0500

    Readded eslint-plugin-import, fixed linting errors, switched to turbopack

--- src/app/(main)/test/page.tsx
+++ src/app/(main)/test/page.tsx
@@ -1,8 +1,8 @@
 import type { FC } from 'react';
 
+import { Counter } from './counter';
 import type { PageComponent } from '@/app/serverComponent';
 import { withSuspense } from '@/withSuspense';
-import { Counter } from './counter';
 
 const TestPage: PageComponent = () => {
   return (
