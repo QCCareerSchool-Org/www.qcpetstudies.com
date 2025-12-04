486b744725bb6cd273f5ff0cd8b1e15951a59135
Dave Welsh <dave@qccareerschool.com>
2025-12-04 11:39:47 -0500

    Updated target, hide sass warnings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  sassOptions: {
+    quietDeps: true, // hide deprecation warnings from node_modules
+    // If your sass-loader / Sass version supports it, you can also do:
+    silenceDeprecations: [ 'import', 'global-builtin' ],
+  },
 };
 
 export default nextConfig;
