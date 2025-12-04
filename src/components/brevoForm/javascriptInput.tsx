ecf5d744c159d43d4df3b377a3744cb9127dce1d
Dave Welsh <dave@qccareerschool.com>
2025-12-04 15:19:08 -0500

    Optimized code

--- src/components/brevoForm/javascriptInput.tsx
+++ src/components/brevoForm/javascriptInput.tsx
@@ -1,22 +1,22 @@
 'use client';
 
 import type { FC } from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef } from 'react';
 
 interface Props {
   name?: string;
 }
 
 export const JavasciptInput: FC<Props> = ({ name }) => {
-  const [ enabled, setEnabled ] = useState(false);
+  const ref = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
-    queueMicrotask(() => {
-      setEnabled(true);
-    });
+    if (ref.current) {
+      ref.current.value = 'enabled';
+    }
   }, []);
 
   return (
-    <input type="hidden" name={name ?? 'javascript'} value={enabled ? 'enabled' : 'disabled'} />
+    <input ref={ref} type="hidden" name={name ?? 'javascript'} value="disabled" />
   );
 };
