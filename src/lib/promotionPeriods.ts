9159b596a537fb20ae62f8ea8a569a2bb7fe48a7
Dave Welsh <dave@qccareerschool.com>
2025-12-23 11:20:37 -0500

    Formatted

--- src/lib/promotionPeriods.ts
+++ src/lib/promotionPeriods.ts
@@ -32,5 +32,12 @@ export class PromotionPeriod {
   public toString = (): string => `[${PromotionPeriod.formatter.format(this.start)}, ${PromotionPeriod.formatter.format(this.end)})`;
 }
 
-export const endOfYear2025 = new PromotionPeriod(Date.UTC(2025, 11, 26, 8), Date.UTC(2026, 0, 3, 8)); // 2025-12-26T03:00:00-0500 to 2026-01-03T03:00:00-0500
-export const newYear2026 = new PromotionPeriod(Date.UTC(2026, 0, 7, 8), Date.UTC(2026, 0, 17, 8)); // 2026-01-07T03:00:00-0500 to 2026-01-17T03:00:00-0500
+export const endOfYear2025 = new PromotionPeriod(
+  Date.UTC(2025, 11, 26, 8), // 2025-12-26T03:00:00-0500
+  Date.UTC(2026, 0, 3, 8), // 2026-01-03T03:00:00-0500
+);
+
+export const newYear2026 = new PromotionPeriod(
+  Date.UTC(2026, 0, 7, 8), // 2026-01-07T03:00:00-0500
+  Date.UTC(2026, 0, 17, 8), // 2026-01-17T03:00:00-0500
+);
