081902a3a6c7d680b8cd58debf3718e973865b89
Dave Welsh <dave@qccareerschool.com>
2025-12-23 14:42:50 -0500

    Added timer

--- src/components/siteLayout/header.tsx
+++ src/components/siteLayout/header.tsx
@@ -1,42 +1,54 @@
-import type { FC } from 'react';
+import { type FC, useMemo } from 'react';
 
 import styles from './header.module.scss';
 import { SecondaryNav } from './secondaryNav';
-import { CountDownTimer } from '@/components/countDownTimer';
+import { CountDownTimer } from '../countDownTimer';
 import { MainNav } from '@/components/siteLayout/mainNav';
 import { gbpCountry } from '@/lib/currencies';
+import { endOfYear2025, newYear2026 } from '@/lib/promotionPeriods';
 
 interface Props {
   date: number;
   countryCode: string;
 }
 
-const bannerStartDate = Date.UTC(2025, 11, 1, 5); // 2025-12-01T00:00 (05:00 UTC)
-const bannerCountdownStartDate = Date.UTC(2025, 11, 5, 8); // 2025-12-05T03:00 (08:00 UTC)
-const bannerEndDate = Date.UTC(2025, 11, 6, 8); // 2025-12-06T03:00 (8:00 UTC)
-
 export const Header: FC<Props> = ({ date, countryCode }) => {
-  const discount = gbpCountry(countryCode) ? '£400' : '$400';
+
+  const dates: [start: number, countDown: number, end: number] | undefined = useMemo(() => {
+    if (endOfYear2025.contains(date)) {
+      return [
+        endOfYear2025.start,
+        endOfYear2025.end - 86_400, // one day
+        endOfYear2025.end,
+      ];
+    } else if (newYear2026.contains(date)) {
+      return [
+        newYear2026.start,
+        newYear2026.end - 86_400, // one day,
+        newYear2026.end,
+      ];
+    }
+  }, [ date ]);
+
+  const discount = endOfYear2025.contains(date)
+    ? (gbpCountry(countryCode) ? '£300' : '$300')
+    : newYear2026.contains(date)
+      ? (gbpCountry(countryCode) ? '£400' : '$400')
+      : undefined;
 
   return (
     <div className={`${styles.wrapper} shadow-lg`}>
-      {/* {countryCode === 'CA' && <CanadaHeader />} */}
-      <CountDownTimer
+      {dates && <CountDownTimer
         date={date}
-        startDate={bannerStartDate}
-        countdownStartDate={bannerCountdownStartDate}
-        endDate={bannerEndDate}
+        startDate={dates[0]}
+        countdownStartDate={dates[1]}
+        endDate={dates[2]}
         message={(
           <span style={{ textTransform: 'uppercase' }}>
-            <span className="d-none d-lg-inline">Don't Miss Out—</span>Cyber Monday Savings Have Arrived! Get {discount} Off Any Course!<br className="d-lg-none" /><button className="btn btn-danger my-2 btn-sm ms-3 text-uppercase">Claim Offer</button>
+            <span className="d-none d-lg-inline">Don't Miss Out—</span> Get {discount} Off<br className="d-lg-none" /><button className="btn btn-danger my-2 btn-sm ms-3 text-uppercase">Claim Offer</button>
           </span>
         )}
-        lastChanceMessage={(
-          <span style={{ textTransform: 'uppercase' }}>
-            <span className="d-none d-lg-inline">Don't Miss Out—</span>Cyber Monday Savings Have Arrived! Get {discount} Off Any Course!<br /><button className="btn btn-danger my-2 btn-sm ms-3 text-uppercase">Claim Offer</button>
-          </span>
-        )}
-      />
+      />}
       <MainNav countryCode={countryCode} />
       <SecondaryNav />
     </div>
