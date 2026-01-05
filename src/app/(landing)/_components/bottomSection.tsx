cfd8627319899f7754f695504b11887453c51c0d
Dave Welsh <dave@qccareerschool.com>
2026-01-05 11:17:14 -0500

    Fixed links

---
+++ src/app/(landing)/_components/bottomSection.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import Link from 'next/link';
+import type { FC, PropsWithChildren, ReactNode } from 'react';
+import { Link as ScrollLink } from 'react-scroll';
+
+interface Props {
+  className?: string;
+  heading?: ReactNode | string;
+  buttonHref?: string;
+  buttonContent?: ReactNode | string;
+  buttonClass?: string;
+}
+
+export const BottomSection: FC<PropsWithChildren<Props>> = ({ className = 'bg-secondary', heading = 'Take the First Step Towards a New and Exciting Career', buttonHref, buttonContent = 'Preview the Course', buttonClass = 'btn-outline-light', children }) => {
+  return (
+    <section className={className}>
+      <div className="container text-center">
+        <div className="row justify-content-center">
+          <div className="col-12 col-lg-10">
+            <h2 className="text-white mb-4">{heading}</h2>
+            {children}
+            {buttonHref
+              ? <Link href={buttonHref} className={`btn ${buttonClass}`}>{buttonContent}</Link>
+              : <ScrollLink to="top" smooth className={`btn ${buttonClass}`}>{buttonContent}</ScrollLink>
+            }
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/app/(landing)/_components/header.tsx
+++ src/app/(landing)/_components/header.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import Link from 'next/link';
-import type { FC, JSX } from 'react';
+import type { FC, ReactNode } from 'react';
+import { Element as ScrollElement } from 'react-scroll';
 
 import { FixedNav } from './fixedNav';
 import styles from './header.module.scss';
@@ -8,16 +11,18 @@ import { Logo } from '@/components/logo';
 interface Props {
   logoLink?: boolean;
   buttonHref?: string;
-  buttonContent?: JSX.Element | string;
+  buttonContent?: ReactNode | string;
 }
 
-export const Header: FC<Props> = ({ logoLink, buttonHref, buttonContent }) => (
-  <div className={styles.wrapper}>
-    <header className={styles.header}>
-      <div className="container">
-        {logoLink ? <Link href="/"><Logo height={28} /></Link> : <Logo height={28} />}
-      </div>
-    </header>
-    <FixedNav buttonHref={buttonHref} buttonContent={buttonContent} />
-  </div>
+export const Header: FC<Props> = ({ logoLink, buttonHref, buttonContent = <><span className="d-none d-sm-inline">Get the </span>Free Course Preview</> }) => (
+  <ScrollElement name="top">
+    <div className={styles.wrapper}>
+      <header className={styles.header}>
+        <div className="container">
+          {logoLink ? <Link href="/"><Logo height={28} /></Link> : <Logo height={28} />}
+        </div>
+      </header>
+      <FixedNav buttonHref={buttonHref} buttonContent={buttonContent} />
+    </div>
+  </ScrollElement>
 );
