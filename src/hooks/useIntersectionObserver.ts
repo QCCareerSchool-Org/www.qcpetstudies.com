ce1829168f7af1a46d2afa9287ce98ea43a9786d
Dave Welsh <dave@qccareerschool.com>
2026-02-09 15:57:21 -0500

    Updated useIntersectionObserver

--- src/hooks/useIntersectionObserver.ts
+++ src/hooks/useIntersectionObserver.ts
@@ -1,26 +1,34 @@
 import type { RefObject } from 'react';
 import { useEffect, useState } from 'react';
 
-export const useIntersectionObserver = (ref: RefObject<HTMLElement | null>, options?: IntersectionObserverInit): boolean => {
+/**
+ * Observes an element to see it is intersected
+ * @param ref A reference to the element being observed
+ * @param options IntersectionObserverInit object
+ * @param once Whether to track only once, the first time the element is intersected; or to keep track of its current intersected state
+ * @returns The state of the intersection
+ */
+export const useIntersectionObserver = (ref: RefObject<HTMLOrSVGElement | null>, options?: IntersectionObserverInit, once?: boolean): boolean => {
   const [ intersected, setIntersected ] = useState(false);
 
   useEffect(() => {
-    if (!ref.current) {
+    const element = ref.current;
+    if (!element) {
       return;
     }
-
-    const currentRef = ref.current;
 
     const observer = new IntersectionObserver(entries => {
       if (entries[0].isIntersecting) {
         setIntersected(true);
+      } else if (!once) {
+        setIntersected(false);
       }
     }, options);
 
-    observer.observe(currentRef);
+    observer.observe(element as HTMLElement);
 
-    return () => { observer.unobserve(currentRef); };
-  }, [ ref, options ]);
+    return () => { observer.unobserve(element as HTMLElement); };
+  }, [ ref, options, once ]);
 
   return intersected;
 };
