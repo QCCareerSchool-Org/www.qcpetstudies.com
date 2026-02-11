4b28f603913b1fc2a3025706fe164d3d543ed8a7
Dave Welsh <dave@qccareerschool.com>
2026-02-11 15:35:47 -0500

    Added banner

--- src/components/siteLayout/header.tsx
+++ src/components/siteLayout/header.tsx
@@ -1,35 +1,35 @@
 import type { FC } from 'react';
 
 import styles from './header.module.scss';
+import { PromoBanner } from './promoBanner';
 import { SecondaryNav } from './secondaryNav';
-import { CountDownTimer } from '../countDownTimer';
 import { MainNav } from '@/components/siteLayout/mainNav';
 import { gbpCountry } from '@/lib/currencies';
-import { feb04 } from '@/lib/promotionPeriods';
+import { feb04, feb13 } from '@/lib/promotionPeriods';
 
 interface Props {
   date: number;
   countryCode: string;
 }
 
-export const Header: FC<Props> = ({ date, countryCode }) => {
-  const discount = gbpCountry(countryCode) ? '£300' : '$300';
+export const Header: FC<Props> = ({ date, countryCode }) => (
+  <div className={`${styles.wrapper} shadow-lg`}>
+    {feb04.contains(date)
+      ? (
+        <PromoBanner date={date} promotionPeriod={feb04.toObject()}>
+          <span className="d-none d-lg-inline">Don't Miss Out—</span>Get {gbpCountry(countryCode) ? '£300' : '$300'} Off Your Tuition!
+        </PromoBanner>
+      )
+      : feb13.contains(date)
+        ? (
+          <PromoBanner date={date} promotionPeriod={feb04.toObject()}>
+            <span className="d-none d-lg-inline">Limited Time Offer:</span> Start today for just {gbpCountry(countryCode) ? '£99' : '$99'} + get a second course free!
 
-  return (
-    <div className={`${styles.wrapper} shadow-lg`}>
-      <CountDownTimer
-        date={date}
-        startDate={feb04.start}
-        countdownStartDate={feb04.lastChance}
-        endDate={feb04.end}
-        message={(
-          <span className="lead text-uppercase">
-            <span className="d-none d-lg-inline">Don't Miss Out—</span> Get {discount} Off Your Tuition!<br className="d-lg-none" /><button className="btn btn-danger my-2 btn-sm ms-3 text-uppercase">Claim Offer</button>
-          </span>
-        )}
-      />
-      <MainNav countryCode={countryCode} />
-      <SecondaryNav />
-    </div>
-  );
-};
+          </PromoBanner>
+        )
+        : null
+    }
+    <MainNav countryCode={countryCode} />
+    <SecondaryNav />
+  </div>
+);
