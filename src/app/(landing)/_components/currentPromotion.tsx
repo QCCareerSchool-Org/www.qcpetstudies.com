f90b22e61e44f1e1cbfa934810a3c7f1841ddd54
Dave Welsh <dave@qccareerschool.com>
2026-02-12 15:33:35 -0500

    Standardized

--- src/app/(landing)/_components/currentPromotion.tsx
+++ src/app/(landing)/_components/currentPromotion.tsx
@@ -1,9 +1,6 @@
 import type { FC } from 'react';
 
-import { FourHundredGroomingTraining } from '@/components/promos/FourHundredGroomingTraining';
-import { NewYears2025 } from '@/components/promos/newYears2025';
 import type { CourseCode } from '@/domain/courseCode';
-import { endOfYear2025, newYear2026 } from '@/lib/periods';
 
 interface Props {
   date: number;
@@ -11,10 +8,4 @@ interface Props {
   courseCode: CourseCode;
 }
 
-export const CurrentPromotion: FC<Props> = ({ date, countryCode, courseCode }) => {
-  if (endOfYear2025.contains(date)) {
-    return <NewYears2025 date={date} countryCode={countryCode} courseCode={courseCode} />;
-  } else if (newYear2026.contains(date)) {
-    return <FourHundredGroomingTraining date={date} countryCode={countryCode} courseCode={courseCode} />;
-  }
-};
+export const CurrentPromotion: FC<Props> = () => null;
