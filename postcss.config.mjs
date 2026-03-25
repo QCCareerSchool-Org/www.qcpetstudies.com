1092439d8a2a68367e452fdf694765bc51360957
Dave Welsh <dave@qccareerschool.com>
2026-03-25 15:29:12 -0400

    Updated purgecss

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -4,11 +4,16 @@ const isProd = process.env.NODE_ENV === 'production';
 const purgeCssOptions = {
   content: [
     './src/app/**/*.{js,ts,jsx,tsx}',
+    './src/app/\\(landing\\)/**/*.{js,ts,jsx,tsx}',
+    './src/app/\\(landing\\)/\\(thank-you\\)/**/*.{js,ts,jsx,tsx}',
+    './src/app/\\(main\\)/**/*.{js,ts,jsx,tsx}',
+    './src/app/\\(terms\\)/**/*.{js,ts,jsx,tsx}',
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
     './node_modules/react-bootstrap/**/*.js',
   ],
   safelist: {
+    standard: [ 'html', 'body', /^PhoneInput/u ],
     deep: [ /modal-/u, /show/u, /fade/u, /nav/u ],
     greedy: [ /^Toastify/u, /^react-multi-carousel/u, /^react-multiple-carousel/u ],
   },
