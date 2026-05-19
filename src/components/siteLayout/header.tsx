b404266eaae1ffe7ba12edbfee04d8128124adc8
Dave Welsh <dave@qccareerschool.com>
2026-05-19 12:45:15 -0400

    Fixed promo period

--- src/components/siteLayout/header.tsx
+++ src/components/siteLayout/header.tsx
@@ -5,7 +5,7 @@ import { PromoBanner } from './promoBanner';
 import { SecondaryNav } from './secondaryNav';
 import { MainNav } from '@/components/siteLayout/mainNav';
 import { gbpCountry } from '@/domain/currency';
-import { may06 } from '@/periods';
+import { may16 } from '@/periods';
 
 interface Props {
   date: number;
@@ -14,11 +14,11 @@ interface Props {
 
 export const Header: FC<Props> = ({ date, countryCode }) => (
   <div className={`${styles.wrapper} shadow-lg`}>
-    {may06.contains(date)
+    {may16.contains(date)
       ? (
-        <PromoBanner date={date} promotionPeriod={may06.toDTO()}>
+        <PromoBanner date={date} promotionPeriod={may16.toDTO()}>
           {countryCode === 'US'
-            ? <>Memorial Week Special: Save $300</>
+            ? <>Memorial Week Special: Save {gbpCountry(countryCode) ? '£300' : '$300'}</>
             : <><span className="d-none d-lg-inline">Limited-time offer: </span>Save {gbpCountry(countryCode) ? '£300' : '$300'} on your tuition when you enroll today!</>
           }
         </PromoBanner>
