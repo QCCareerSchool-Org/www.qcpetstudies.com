e1b71c1359bf62bdaa085c82bb3dad0a695f3796
Angela Li <angelayxli@gmail.com>
2026-06-18 13:05:26 -0400

    fixed testimonial

--- src/app/(main)/grooming-career-guide/400-off/page.tsx
+++ src/app/(main)/grooming-career-guide/400-off/page.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     canonical: '/grooming-career-guide',
   },
 };
-const testimonialIds = [ 'TD-0004', 'TD-0005', 'TD-0007', 'TD-0008', 'TD-0009', 'TD-0010' ];
+const testimonialIds = [ 'TD-0004', 'TD-0005', 'TD-0007', 'TD-0014', 'TD-0009', 'TD-0010' ];
 
 const CareerGuidePage: PageComponent = () => {
   return (
