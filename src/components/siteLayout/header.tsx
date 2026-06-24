3e07ec01cdcd50334f97b9b297edd8fc57d30672
Angela Li <angelayxli@gmail.com>
2026-06-24 11:00:50 -0400

    schedule site wide banner

--- src/components/siteLayout/header.tsx
+++ src/components/siteLayout/header.tsx
@@ -6,7 +6,7 @@ import { SecondaryNav } from './secondaryNav';
 import { CanadaHeader } from '../canadaHeader';
 import { MainNav } from '@/components/siteLayout/mainNav';
 import { gbpCountry } from '@/domain/currency';
-import { june13 } from '@/periods';
+import { june13, june27 } from '@/periods';
 
 interface Props {
   date: number;
@@ -28,9 +28,9 @@ const InnerBanner: FC<Props> = ({ date, countryCode }) => {
 
   if (june13.contains(date)) {
     return (
-      <PromoBanner date={date} promotionPeriod={june13.toDTO()}>
+      <PromoBanner date={date} promotionPeriod={june27.toDTO()}>
         {countryCode === 'US'
-          ? <>Ends Soon: Save $400 On Tuition</>
+          ? <>4th of July Special: Save $400 on Tuition</>
           : <><span className="d-none d-lg-inline">Limited-time offer:{' '}</span>Save {gbpCountry(countryCode) ? '£400' : '$400'} on your tuition when you enroll today!</>
         }
       </PromoBanner>
