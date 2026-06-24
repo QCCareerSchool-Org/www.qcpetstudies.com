021458056ed51d201d7a53b0d6b1b9b09ac4abdb
Angela Li <angelayxli@gmail.com>
2026-06-24 11:03:04 -0400

    keep old banner up until 27th

--- src/components/siteLayout/header.tsx
+++ src/components/siteLayout/header.tsx
@@ -28,6 +28,16 @@ const InnerBanner: FC<Props> = ({ date, countryCode }) => {
 
   if (june13.contains(date)) {
     return (
+      <PromoBanner date={date} promotionPeriod={june13.toDTO()}>
+        {countryCode === 'US'
+          ? <>Ends Soon: Save $400 On Tuition</>
+          : <><span className="d-none d-lg-inline">Limited-time offer:{' '}</span>Save {gbpCountry(countryCode) ? '£400' : '$400'} on your tuition when you enroll today!</>
+        }
+      </PromoBanner>
+    );
+  }
+  if (june27.contains(date)) {
+    return (
       <PromoBanner date={date} promotionPeriod={june27.toDTO()}>
         {countryCode === 'US'
           ? <>4th of July Special: Save $400 on Tuition</>
