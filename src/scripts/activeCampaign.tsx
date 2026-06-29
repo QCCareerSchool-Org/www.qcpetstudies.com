39dd041643e29f1bf8bad96425070437887d0db1
Dave Welsh <dave@qccareerschool.com>
2026-06-29 15:47:20 -0400

    Changed order of execution

--- src/scripts/activeCampaign.tsx
+++ src/scripts/activeCampaign.tsx
@@ -10,21 +10,15 @@ interface Props {
 }
 
 export const ActiveCampaign: FC<Props> = ({ id, userValues }) => (
-  <>
-    <Script id="activeCampaign" dangerouslySetInnerHTML={{ __html: getScript(id) }} />
-    {userValues && <Script id="activeCampaignVisitor" dangerouslySetInnerHTML={{ __html: getVisitorScript(userValues) }} />}
-  </>
+  <Script id="activeCampaign" dangerouslySetInnerHTML={{ __html: getScript(id, userValues) }} />
 );
 
-const getScript = (id: string): string => `
+const getScript = (id: string, userValues?: UserValues): string => `
 (function(e,t,o,n,p,r,i){e.visitorGlobalObjectAlias=n;e[e.visitorGlobalObjectAlias]=e[e.visitorGlobalObjectAlias]||function(){(e[e.visitorGlobalObjectAlias].q=e[e.visitorGlobalObjectAlias].q||[]).push(arguments)};e[e.visitorGlobalObjectAlias].l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)})(window,document,"https://diffuser-cdn.app-us1.com/diffuser/diffuser.js","vgo");
 vgo('setAccount', ${safeJSON(id)});
 vgo('setTrackByDefault', true);
-vgo('process');
-`;
-
-const getVisitorScript = (userValues: UserValues) => `
-if (${safeJSON(userValues.emailAddress)}) {
-  vgo('setEmail', ${safeJSON(userValues.emailAddress)});
+if (${safeJSON(userValues?.emailAddress)}) {
+  vgo('setEmail', ${safeJSON(userValues?.emailAddress)});
 }
+vgo('process');
 `;
