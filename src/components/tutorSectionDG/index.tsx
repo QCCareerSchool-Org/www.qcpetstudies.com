b8aa7cf2b29d1d0b0f94edcfc0e795a570fa1ac4
Angela Li <angelayxli@gmail.com>
2026-07-09 09:07:36 -0400

    replace tutor achor with names

--- src/components/tutorSectionDG/index.tsx
+++ src/components/tutorSectionDG/index.tsx
@@ -8,9 +8,9 @@ interface Props {
   id?: string;
 }
 
-export const TutorSectionDG: FC<Props> = ({ className, id = 'tutors' }) => (
+export const TutorSectionDG: FC<Props> = ({ className }) => (
   <>
-    <div id={id} className="sectionAnchor" />
+    <div className="sectionAnchor" />
     <section className={className}>
       <div className="container text-center">
         <div className="row justify-content-center">
@@ -18,13 +18,13 @@ export const TutorSectionDG: FC<Props> = ({ className, id = 'tutors' }) => (
             <h2>Meet Your Grooming Mentors</h2>
             <p>When you enroll with QC Pet Studies, you'll be paired with an expert grooming mentor who will review your work and provide detailed audio feedback on each assignment. With decades of industry experience, your mentor will offer valuable advice to help you succeed and improve as you progress through the course.</p>
             <div className="row justify-content-center g-5 g-lg-5 mt-4">
-              <div className="col-12 col-lg-4">
+              <div className="col-12 col-lg-4" id="lisa">
                 <MasterIcon {...lisaDay} />
               </div>
-              <div className="col-12 col-lg-4">
+              <div className="col-12 col-lg-4" id="paddy">
                 <MasterIcon {...paddyGaffney} />
               </div>
-              <div className="col-12 col-lg-4">
+              <div className="col-12 col-lg-4" id="casey">
                 <MasterIcon {...caseyBechard} />
               </div>
             </div>
