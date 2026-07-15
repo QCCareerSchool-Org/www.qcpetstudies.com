de25756778e1d2662b29267816269c19c1307f50
Angela Li <angelayxli@gmail.com>
2026-07-15 10:11:58 -0400

    implement new footer

--- src/app/(landing)/_components/footer.tsx
+++ src/app/(landing)/_components/footer.tsx
@@ -1,6 +1,8 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import type { FC } from 'react';
 
+import PhoneCallIcon from './phone-call.svg';
 import { TelephoneLink } from '@/components/telephoneLink';
 import { gbpCountry } from '@/domain/currency';
 import { getServerData } from '@/lib/getServerData';
@@ -13,9 +15,24 @@ export const Footer: FC = async () => {
     <footer className="bg-light">
       <section>
         <div className="container text-center">
-          <h2>Have questions?<br />Give us a call.</h2>
-          <p className="lead"><TelephoneLink countryCode={countryCode} /></p>
-          &copy;{new Date().getFullYear()} QC Pet Studies<br /><Link href={termsLink}>Privacy Policy</Link>
+          <div className="row justify-content-center">
+            <div className="col-12 col-lg-10">
+              <h2 className="fw-bold mb-4">Have questions?</h2>
+              <h2 className="mb-4 fw-bold" style={{ fontSize: 'clamp(1.5rem, 2.1vw, 2.15rem)' }}>Give us a call</h2>
+              <div className="d-flex align-items-center justify-content-center rounded-circle bg-primary p-4 mb-4 mx-auto" style={{ width: 'fit-content' }}>
+                <Image src={PhoneCallIcon} alt="" width={40} height={40} />
+              </div>
+              <p className="mb-4 fw-bold" style={{ fontSize: 'clamp(1.5rem, 2.1vw, 2.15rem)' }}><TelephoneLink countryCode={countryCode} className="text-dark" /></p>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <hr />
+        </div>
+        <div>
+          <div className="container d-flex justify-content-between align-items-center">
+            &copy;{new Date().getFullYear()} QC Pet Studies<br /><Link href={termsLink}>Privacy Policy</Link>
+          </div>
         </div>
       </section>
     </footer>
