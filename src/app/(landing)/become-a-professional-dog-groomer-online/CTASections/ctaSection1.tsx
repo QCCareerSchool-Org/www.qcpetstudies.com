b6020bd1779f3a22c95536d31de29b9f0b77eb9f
Angela Li <angelayxli@gmail.com>
2026-07-22 13:16:26 -0400

    remove icons and rename url

--- src/app/(landing)/become-a-professional-dog-groomer-online/CTASections/ctaSection1.tsx
+++ src/app/(landing)/become-a-professional-dog-groomer-online/CTASections/ctaSection1.tsx
@@ -1,9 +1,6 @@
-import Image from 'next/image';
 import type { FC } from 'react';
 
-import BBB from '../bbb.svg';
 import styles from './cta.module.scss';
-import Guarantee from '../guarantee.svg';
 
 export const CTASection1: FC = () => (
   <section className={styles.ctaBand}>
@@ -13,16 +10,6 @@ export const CTASection1: FC = () => (
           <h2 className="text-white mb-3">Ready to Launch Your Career?</h2>
           <div className="d-flex justify-content-center gap-3 flex-wrap mb-4">
             <a href="#request-info" className="btn btn-primary btn-lg">Preview the Course</a>
-          </div>
-          <div className="d-flex justify-content-center gap-4 flex-wrap">
-            <div className="d-flex flex-column align-items-center gap-3">
-              <Image src={BBB} alt="BBB Accredited" width={50} height={50} />
-              <div className="fw-bold">BBB ACCREDITED</div>
-            </div>
-            <div className="d-flex flex-column align-items-center gap-3">
-              <Image src={Guarantee} alt="14-Day Guarantee" width={50} height={50} />
-              <div className="fw-bold">14-DAY GUARANTEE</div>
-            </div>
           </div>
         </div>
       </div>
