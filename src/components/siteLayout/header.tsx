9613cfc31f779d1ba98a995af2f1ad33cf0510aa
Angela Li <angelayxli@gmail.com>
2026-08-04 10:30:02 -0400

    schedule site wide banner

--- src/components/siteLayout/header.tsx
+++ src/components/siteLayout/header.tsx
@@ -5,7 +5,7 @@ import { PromoBanner } from './promoBanner';
 import { SecondaryNav } from './secondaryNav';
 import { MainNav } from '@/components/siteLayout/mainNav';
 import { gbpCountry } from '@/domain/currency';
-import { july08, july22 } from '@/periods';
+import { august06, july22 } from '@/periods';
 
 interface Props {
   date: number;
@@ -22,10 +22,10 @@ export const Header: FC<Props> = props => (
 
 const InnerBanner: FC<Props> = ({ date, countryCode }) => {
 
-  if (july08.contains(date)) {
+  if (august06.contains(date)) {
     return (
-      <PromoBanner date={date} promotionPeriod={july08.toDTO()}>
-        <span className="d-none d-lg-inline">Limited-time offer:{' '}</span>Save {gbpCountry(countryCode) ? '£400' : '$400'} on your tuition when you enroll today!
+      <PromoBanner date={date} promotionPeriod={august06.toDTO()}>
+        <span className="d-none d-lg-inline">Flash offer:{' '}</span>Enroll Today & Get a 2nd Course Free
       </PromoBanner>
     );
   }
